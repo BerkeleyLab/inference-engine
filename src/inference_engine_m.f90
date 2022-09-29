518b259df7536617d22f76654067968f86295f89
Damian Rouson <rouson@lbl.gov>
2022-09-29 16:01:16 -0700

    feat: initial inference engine type & constructors

---
+++ src/inference_engine_m.f90
@@ -0,0 +1,40 @@
+module inference_engine_m
+  !! Define an abstraction that supports inference operationsn on a neural network
+  implicit none
+
+  private
+  public :: inference_engine_t
+
+  type inference_engine_t
+    !! Encapsulate the minimal information needed to performance inference
+    private
+    real, allocatable :: input_weights_(:,:)
+    real, allocatable :: hidden_weights_(:,:,:)
+    real, allocatable :: output_weights_(:,:)
+  contains
+    generic :: inference_engine_t => read_weights
+    procedure :: read_weights
+  end type
+
+  interface inference_engine_t
+
+    pure module function construct(input_weights, output_weights, hidden_weights) result(inference_engine)
+      implicit none
+      real, intent(in), dimension(:,:) :: input_weights, output_weights
+      real, intent(in) :: hidden_weights(:,:,:)
+      type(inference_engine_t) inference_engine
+    end function
+
+  end interface
+
+  interface
+
+    module subroutine read_weights(self, file_name)
+      implicit none
+      class(inference_engine_t), intent(out) :: self
+      character(len=*), intent(in) :: file_name
+    end subroutine
+
+  end interface
+
+end module inference_engine_m
