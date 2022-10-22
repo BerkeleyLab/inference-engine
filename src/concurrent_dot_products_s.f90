6edab4d231e1453e61639fdcf2cf5f042a1a675f
Damian Rouson <rouson@lbl.gov>
2022-10-21 18:43:39 -0700

    refac(infer): polymorphic activation strategy

--- src/concurrent_dot_products_s.f90
+++ src/concurrent_dot_products_s.f90
@@ -1,5 +1,6 @@
 submodule(concurrent_dot_products_m) concurrent_dot_products_s
   use assert_m, only : assert
+  use activation_strategy_m, only : activation_strategy_t
   use step_m, only : step_t
   implicit none
 
@@ -11,20 +12,22 @@ contains
     integer, parameter :: input_layer = 1
     real, allocatable :: neuron(:,:)
 
-    type(step_t) :: step
+    class(activation_strategy_t), allocatable :: activation_strategy
+    allocate(step_t :: activation_strategy)
    
     allocate(neuron(neurons_per_layer, num_layers))
     do concurrent(n = 1:neurons_per_layer)
-      neuron(n,input_layer) = step%activation(dot_product(input_weights(:,n), input(:)) + biases(n,input_layer))
+      neuron(n,input_layer) = activation_strategy%activation(dot_product(input_weights(:,n), input(:)) + biases(n,input_layer))
     end do
     do layer = 2, num_layers
       do concurrent(n = 1:neurons_per_layer)
-        neuron(n,layer) = step%activation(dot_product(hidden_weights(:,n,layer-1), neuron(:,layer-1)) + biases(n,layer))
+        neuron(n,layer) = &
+          activation_strategy%activation(dot_product(hidden_weights(:,n,layer-1), neuron(:,layer-1)) + biases(n,layer))
       end do
     end do
     allocate(output(num_outputs))
     do concurrent(m = 1:num_outputs)
-      output(m) = step%activation(dot_product(output_weights(m,:), neuron(:,num_layers)) + output_biases(m))
+      output(m) = activation_strategy%activation(dot_product(output_weights(m,:), neuron(:,num_layers)) + output_biases(m))
     end do
 
   end procedure
