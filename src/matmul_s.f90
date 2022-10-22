802cf61a1c53886f59b99ae701103d221d20c54f
Damian Rouson <rouson@lbl.gov>
2022-10-21 23:27:15 -0700

    feat(matmul): complete matmul inference option

--- src/matmul_s.f90
+++ src/matmul_s.f90
@@ -5,22 +5,24 @@ contains
 
   module procedure infer
     
-    integer n, layer, m
-    integer, parameter :: input_layer = 1
     real, allocatable :: neuron(:,:)
 
-    associate(neurons_per_layer => size(input_weights,2), num_layers => size(biases,2))
-      allocate(neuron(neurons_per_layer, num_layers))
-      do concurrent(n = 1:neurons_per_layer)
-        neuron(n,input_layer) = activation_strategy%activation(dot_product(input_weights(:,n), input(:)) + biases(n,input_layer))
-      end do
-      do layer = 2, num_layers
-        do concurrent(n = 1:neurons_per_layer)
-          neuron(n,layer) = &
-            activation_strategy%activation(dot_product(hidden_weights(:,n,layer-1), neuron(:,layer-1)) + biases(n,layer))
+    associate(num_layers => size(biases,2))
+      associate(neurons_per_layer => size(input_weights,2))
+        allocate(neuron(neurons_per_layer, num_layers))
+      end associate
+      block
+        integer, parameter :: input_layer = 1
+        neuron(:,input_layer) = &
+          activation_strategy%activation(matmul(transpose(input_weights(:,:)), input(:)) + biases(:,input_layer))
+      end block
+      block
+        integer layer
+        do layer = 2, num_layers
+          neuron(:,layer) = &
+            activation_strategy%activation(matmul(hidden_weights(:,:,layer-1), neuron(:,layer-1)) + biases(:,layer))
         end do
-      end do
-
+      end block
       output = activation_strategy%activation(matmul(output_weights(:,:), neuron(:,num_layers)) + output_biases(:))
     end associate
 
