7ab624b55f0664ce63f1bb62ded0d9849fc0efdf
Damian Rouson <rouson@lbl.gov>
2023-02-11 14:31:16 -0800

    refac(infer): rm unnecessary alloc attributes
    This will facilitate passing in expressions and will thus prove
    useful in the transition to avoiding repeated matrix transposes
    inside the inference functions by passing in a transposed matrix.

--- src/matmul_m.f90
+++ src/matmul_m.f90
@@ -22,12 +22,12 @@ module matmul_m
       input, input_weights, hidden_weights, biases, output_biases, output_weights, activation_strategy &
     ) result(output)
       implicit none
-      real(rkind), intent(in)  :: input(:)
-      real(rkind), intent(in), allocatable :: input_weights(:,:)    !! weights applied to go from the inputs to first hidden layer
-      real(rkind), intent(in), allocatable :: hidden_weights(:,:,:) !! weights applied to go from one hidden layer to the next
-      real(rkind), intent(in), allocatable :: biases(:,:)           !! neuronal offsets for each hidden layer
-      real(rkind), intent(in), allocatable :: output_biases(:)      !! neuronal offsets applied to outputs
-      real(rkind), intent(in), allocatable :: output_weights(:,:)   !! weights applied to go from the final hidden layer to the outputs
+      real(rkind), intent(in) :: input(:)
+      real(rkind), intent(in) :: input_weights(:,:)    !! weights applied to go from the inputs to first hidden layer
+      real(rkind), intent(in) :: hidden_weights(:,:,:) !! weights applied to go from one hidden layer to the next
+      real(rkind), intent(in) :: biases(:,:)           !! neuronal offsets for each hidden layer
+      real(rkind), intent(in) :: output_biases(:)      !! neuronal offsets applied to outputs
+      real(rkind), intent(in) :: output_weights(:,:)   !! weights applied to go from the final hidden layer to the outputs
       class(activation_strategy_t), intent(in) :: activation_strategy
       real(rkind), allocatable :: output(:)
     end function
