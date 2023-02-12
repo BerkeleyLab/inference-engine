8dc5526ea0eb4b28fe6b40646f3f562eb77720d7
rouson <rouson@lbl.gov>
2023-02-12 02:53:21 +0000

    Deploying to gh-pages from @ BerkeleyLab/inference-engine@3179b7f09099f64fd4d468bde2a89dbc8b1e8f04 🚀

--- src/matmul_s.f90
+++ src/matmul_s.f90
@@ -10,19 +10,20 @@ contains
     real(rkind), allocatable :: neuron(:,:)
 
     associate(num_layers => size(biases,2))
-      associate(neurons_per_layer => size(input_weights,2))
+
+      associate(neurons_per_layer => size(input_weights,1))
         allocate(neuron(neurons_per_layer, num_layers))
       end associate
       block
         integer, parameter :: input_layer = 1
         neuron(:,input_layer) = &
-          activation_strategy%activation(matmul(transpose(input_weights(:,:)), input(:)) + biases(:,input_layer))
+          activation_strategy%activation(matmul(input_weights(:,:), input(:)) + biases(:,input_layer))
       end block
       block
         integer layer
         do layer = 2, num_layers
           neuron(:,layer) = &
-            activation_strategy%activation(matmul(transpose(hidden_weights(:,:,layer-1)), neuron(:,layer-1)) + biases(:,layer))
+            activation_strategy%activation(matmul(hidden_weights(:,:,layer-1), neuron(:,layer-1)) + biases(:,layer))
         end do
       end block
       output = activation_strategy%activation(matmul(output_weights(:,:), neuron(:,num_layers)) + output_biases(:))
