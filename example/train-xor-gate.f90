3727b009edd69d6e8fca0ea7891789276825919e
Damian Rouson <rouson@lbl.gov>
2023-05-25 14:27:40 +0200

    fix(example): rm dead code; reduce execution time
    In the example/train-xor-gate.f90, this commit increases the width
    of the network in example/train-xor-gate.f90 to minimize runtime
    by trading increased work per training iteration for a reduction
    in the number of iterations required to converge.  This change
    reduces the execution time by nearly an order of magnitude.

--- example/train-xor-gate.f90
+++ example/train-xor-gate.f90
@@ -18,7 +18,7 @@ program train_xor_gate
 
   real(rkind), parameter :: tolerance = 1.E-02_rkind, false = 0._rkind, true = 1._rkind
   type(outputs_t), dimension(4) :: actual_output
-  type(expected_outputs_t), dimension(4) :: expected_outputs 
+  type(expected_outputs_t), dimension(size(actual_output)) :: expected_outputs 
   type(trainable_engine_t) trainable_engine
   type(command_line_t) command_line
   type(string_t) base_name
@@ -41,26 +41,41 @@ program train_xor_gate
     expected_outputs_t([false]), expected_outputs_t([true]), expected_outputs_t([true]), expected_outputs_t([false]) &
   ]
   print *,"Defining mini-batches, each containing input/output pairs corresponding to the four entries in the XOR truth table."
-  mini_batches = [(mini_batch_t( input_output_pair_t( inputs, expected_outputs ) ), m=1,2000000)]
+  mini_batches = [(mini_batch_t( input_output_pair_t( inputs, expected_outputs ) ), m=1,110000)]
   print *,"Defining an initial trainable_engine_t neural network object."
   trainable_engine = wide_single_layer_perceptron()
-  print *,"Training the neural network using the mini-batches. ___This could take a few minutes.___"
-  call trainable_engine%train(mini_batches, matmul_t())
+  print *,"Training the neural network using the mini-batches."
+  block
+    real t_start, t_end
+    call cpu_time(t_start)
+    call trainable_engine%train(mini_batches, matmul_t())
+    call cpu_time(t_end)
+    print *,"Training time:", t_end - t_start
+  end block
   associate(file_name => string_t(base_name%string() // ".json"))
     print *,"Writing the network parameters to "
     call output(trainable_engine, file_name)
   end associate
   print *,"Verifying that the network behaves as an exclusive-or (XOR) logic gate: "
   actual_output = trainable_engine%infer(inputs, matmul_t())
-  if (any([(abs(actual_output(i)%outputs() - expected_outputs(i)%outputs()) < tolerance, i=1, size(actual_output))])) then
+  if (all([(abs(actual_output(i)%outputs() - expected_outputs(i)%outputs()) < tolerance, i=1, size(actual_output))])) then
     print *,"Yes!"
+    call print_diagnostics
   else
+    call print_diagnostics
     error stop &
       "The trained network does not behave as a XOR gate.  " // &
       "Please report this issue at https://github.com/BerkeleyLab/inference-engine/issues."
   end if
 
 contains
+
+  subroutine print_diagnostics()
+    print *,"Network output: ", (actual_output(i)%outputs(), i=1,size(actual_output))
+    print *,"Expected output: ", (expected_outputs(i)%outputs(), i=1,size(expected_outputs))
+    print *,"Absolute Error: ", (abs(actual_output(i)%outputs() - expected_outputs(i)%outputs()), i = 1, size(actual_output))
+    print *,"Tolerance: ", tolerance
+  end subroutine
 
   subroutine output(engine, file_name)
     type(trainable_engine_t), intent(in) :: engine
@@ -76,27 +91,7 @@ contains
     type(trainable_engine_t) trainable_engine
     integer, parameter :: n_in = 2 ! number of inputs
     integer, parameter :: n_out = 1 ! number of outputs
-    integer, parameter :: neurons = 3 ! number of neurons per layer
-    integer, parameter :: n_hidden = 1 ! number of hidden layers 
-   
-    trainable_engine = trainable_engine_t( &
-      metadata = [ &
-       string_t("Trainable XOR"), string_t("Damian Rouson"), string_t("2023-05-09"), string_t("sigmoid"), string_t("false") &
-      ], &
-      input_weights = real(reshape([1,0,1,1,0,1], [n_in, neurons]), rkind), &
-      hidden_weights = reshape([real(rkind)::], [neurons,neurons,n_hidden-1]), &
-      output_weights = real(reshape([1,-2,1], [n_out, neurons]), rkind), &
-      biases = reshape([real(rkind):: 0.,-1.99,0.], [neurons, n_hidden]), &
-      output_biases = [real(rkind):: 0.], &
-      differentiable_activation_strategy = sigmoid_t() &
-    )
-  end function
-
-  function wide_perceptron() result(trainable_engine)
-    type(trainable_engine_t) trainable_engine
-    integer, parameter :: n_in = 2 ! number of inputs
-    integer, parameter :: n_out = 1 ! number of outputs
-    integer, parameter :: neurons = 24 ! number of neurons per layer
+    integer, parameter :: neurons = 36 ! number of neurons per layer
     integer, parameter :: n_hidden = 1 ! number of hidden layers 
     integer n
    
@@ -104,10 +99,10 @@ contains
       metadata = [ &
        string_t("Wide 1-layer perceptron"), string_t("Damian Rouson"), string_t("2023-05-24"), string_t("sigmoid"), string_t("false") &
       ], &
-      input_weights = real(reshape([([1,0,1,1,0,1], n=1,8 )], [n_in, neurons]), rkind), &
+      input_weights = real(reshape([([1,0,1,1,0,1], n=1,12 )], [n_in, neurons]), rkind), &
       hidden_weights = reshape([real(rkind)::], [neurons,neurons,n_hidden-1]), &
-      output_weights = real(reshape([([1,-2,1], n=1,8)], [n_out, neurons]), rkind), &
-      biases = reshape([real(rkind):: [(0.,-1.99,0., n=1,8)] ], [neurons, n_hidden]), &
+      output_weights = real(reshape([([1,-2,1], n=1,12)], [n_out, neurons]), rkind), &
+      biases = reshape([real(rkind):: [(0.,-1.99,0., n=1,12)] ], [neurons, n_hidden]), &
       output_biases = [real(rkind):: 0.], &
       differentiable_activation_strategy = sigmoid_t() &
     )
