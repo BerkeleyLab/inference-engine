508a671fd1acc512abc3e17dece2860eda95d643
Damian Rouson <rouson@lbl.gov>
2023-06-29 19:23:48 +0200

    test(train): skewed input reduces test runtime

--- test/trainable_engine_test_m.f90
+++ test/trainable_engine_test_m.f90
@@ -26,6 +26,8 @@ module trainable_engine_test_m
     procedure, nopass :: results
   end type
 
+  real(rkind), parameter :: false = 0._rkind, true = 1._rkind
+
 contains
 
   pure function subject() result(specimen)
@@ -37,189 +39,133 @@ contains
     type(test_result_t), allocatable :: test_results(:)
 
     character(len=*), parameter :: longest_description = &
-        "learning the mapping (false,false) -> false when trained on a fixed input/output pair"
+        "learning the mapping (false,false) -> false with 2 hidden layers trained on AND-gate input/output pairs"
 
     associate( &
       descriptions => &
       [ character(len=len(longest_description)) :: &
-        "learning the mapping (true,true) -> false when trained on a fixed input/output pair", &
-        "learning the mapping (false,true) -> true when trained on a fixed input/output pair", &
-        "learning the mapping (true,false) -> true when trained on a fixed input/output pair", &
-        "learning the mapping (false,false) -> false when trained on a fixed input/output pair", &
-        "learning the mapping (true,true) -> false trained on mini-batches", &
-        "learning the mapping (false,true) -> true trained on mini-batches", &
-        "learning the mapping (true,false) -> true trained on mini-batches", &
-        "learning the mapping (false,false) -> false trained on mini-batches", &
-        "learning the mapping (true,true) -> true using two hidden layers", &
-        "learning the mapping (false,true) -> false using two hidden layers", &
-        "learning the mapping (true,false) -> false using two hidden layers", &
-        "learning the mapping (false,false) -> false using two hidden layers" &
+        "learning the mapping (true,true) -> true with 2 hidden layers trained on AND-gate input/output pairs", &
+        "learning the mapping (false,true) -> false with 2 hidden layers trained on AND-gate input/output pairs", &
+        "learning the mapping (true,false) -> false with 2 hidden layers trained on AND-gate input/output pairs", &
+        "learning the mapping (false,false) -> false with 2 hidden layers trained on AND-gate input/output pairs", &
+        "learning the mapping (true,true) -> false with 2 hidden layers trained on NOT_AND-gate input/output pairs", &
+        "learning the mapping (false,true) -> true with 2 hidden layers trained on NOT-AND-gate input/output pairs", &
+        "learning the mapping (true,false) -> true with 2 hidden layers trained on NOT-AND-gate input/output pairs", &
+        "learning the mapping (false,false) -> true with 2 hidden layers trained on NOT-AND-gate input/output pairs" &
       ], outcomes => [ &
-        train_on_fixed_input_output_pair(), &
-        train_on_xor_truth_table_mini_batch(), &
-        train_on_and_truth_table_mini_batch() &
+        train_and_gate(), &
+        train_not_and_gate() &
       ] &
     )
-      call assert(size(descriptions) == size(outcomes), "trainable_engine_test_m(results): size(descritions) == size(outcomes)")
+      call assert(size(descriptions) == size(outcomes), "trainable_engine_test_m(results): size(descriptions) == size(outcomes)")
       test_results = test_result_t(descriptions, outcomes)
     end associate
   end function
 
-  function trainable_hidden_layer() result(trainable_engine)
+  function two_random_hidden_layers() result(trainable_engine)
     type(trainable_engine_t) trainable_engine
-    integer, parameter :: n_in = 2 ! number of inputs
-    integer, parameter :: n_out = 1 ! number of outputs
-    integer, parameter :: neurons = 3 ! number of neurons per layer
-    integer, parameter :: n_hidden = 1 ! number of hidden layers 
+    integer, parameter :: inputs = 2, outputs = 1, hidden = 3 ! number of neurons in input, output, and hidden layers
+    integer, parameter :: neurons(*) = [inputs, hidden, hidden, outputs] ! neurons per layer
+    integer, parameter :: max_neurons = maxval(neurons), layers=size(neurons) ! max layer width, number of layers
+    real(rkind) w(max_neurons, max_neurons, layers-1), b(max_neurons, max_neurons)
+
+    w = 0.
+    b = 0.
    
     trainable_engine = trainable_engine_t( &
-      metadata = [ &
-       string_t("Trainable XOR"), string_t("Damian Rouson"), string_t("2023-05-09"), string_t("sigmoid"), string_t("false") &
-      ], &
-      input_weights = real(reshape([1,0,1,1,0,1], [n_in, neurons]), rkind), &
-      hidden_weights = reshape([real(rkind)::], [neurons,neurons,n_hidden-1]), &
-      output_weights = real(reshape([1,-2,1], [n_out, neurons]), rkind), &
-      biases = reshape([real(rkind):: 0.,-1.99,0.], [neurons, n_hidden]), &
-      output_biases = [real(rkind):: 0.], &
-      differentiable_activation_strategy = sigmoid_t() &
-    )
-  end function
-
-  function train_on_fixed_input_output_pair() result(test_passes)
-    logical, allocatable :: test_passes(:)
-    real(rkind), parameter :: tolerance = 1.E-02_rkind, false = 0._rkind, true = 1._rkind
-    type(outputs_t), dimension(4) :: actual_output
-    type(expected_outputs_t), dimension(4) :: expected_outputs !gfortran doesn't allow replacing with an association
-    type(trainable_engine_t) trainable_engine
-    integer i, j
-
-    expected_outputs = [ &
-      expected_outputs_t([false]), expected_outputs_t([true]), expected_outputs_t([true]), expected_outputs_t([false]) &
-    ] 
-    associate( &
-      inputs => [ &
-        inputs_t([true,true]), inputs_t([false,true]), inputs_t([true,false]), inputs_t([false,false]) &
-      ] &
-    )
-     loop_over_truth_table_entries: &
-      do j =1, size(inputs)
-        trainable_engine = trainable_hidden_layer()
-        call trainable_engine%train([(mini_batch_t(input_output_pair_t([inputs(j)], [expected_outputs(j)])), i=1,3000)], matmul_t())
-        actual_output(j) = trainable_engine%infer(inputs(j), matmul_t())
-      end do loop_over_truth_table_entries
-      test_passes = [(abs(actual_output(j)%outputs() - expected_outputs(j)%outputs()) < tolerance, j=1, size(inputs))]
-    end associate
-  end function
-
-  function wide_hidden_layer() result(trainable_engine)
-    type(trainable_engine_t) trainable_engine
-    integer, parameter :: n_in = 2 ! number of inputs
-    integer, parameter :: n_out = 1 ! number of outputs
-    integer, parameter :: neurons = 36 ! number of neurons per layer
-    integer, parameter :: n_hidden = 1 ! number of hidden layers 
-    integer n
-   
-    trainable_engine = trainable_engine_t( &
-      metadata = [ & 
-       string_t("Wide 1-layer network"), string_t("Damian Rouson"), string_t("2023-05-24"), string_t("sigmoid"), string_t("false") &
-      ], &
-      input_weights = real(reshape([([1,0,1,1,0,1], n=1,12 )], [n_in, neurons]), rkind), &
-      hidden_weights = reshape([real(rkind)::], [neurons,neurons,n_hidden-1]), &
-      output_weights = real(reshape([([1,-2,1], n=1,12)], [n_out, neurons]), rkind), &
-      biases = reshape([real(rkind):: [(0.,-1.99,0., n=1,12)] ], [neurons, n_hidden]), &
-      output_biases = [real(rkind):: 0.], &
-      differentiable_activation_strategy = sigmoid_t() &
+      nodes = neurons, weights = w, biases = b, differentiable_activation_strategy = sigmoid_t(), metadata = &
+      [string_t("2-hide|3-wide"), string_t("Damian Rouson"), string_t("2023-06-28"), string_t("sigmoid"), string_t("false")] &
     )   
   end function
 
-  function train_on_xor_truth_table_mini_batch() result(test_passes)
-    logical, allocatable :: test_passes(:)
-    type(trainable_engine_t) trainable_engine
-    real(rkind), parameter :: tolerance = 1.E-02_rkind, false = 0._rkind, true = 1._rkind
-    type(outputs_t), allocatable :: actual_output(:)
-    type(inputs_t), allocatable :: inputs(:)
-    type(expected_outputs_t), allocatable :: expected_outputs(:) !gfortran doesn't allow replacing with an association
-    type(mini_batch_t), allocatable :: mini_batches(:) !gfortran doesn't allow replacing with an association
-    integer i, m
-
-    inputs = [ & 
-      inputs_t([true,true]), inputs_t([false,true]), inputs_t([true,false]), inputs_t([false,false]) &
-    ]
-    expected_outputs = [ & 
-      expected_outputs_t([false]), expected_outputs_t([true]), expected_outputs_t([true]), expected_outputs_t([false]) &
-    ]
-    mini_batches = [(mini_batch_t( input_output_pair_t( inputs, expected_outputs ) ), m=1,100000)]
-    trainable_engine = wide_hidden_layer()
-    call trainable_engine%train(mini_batches, matmul_t())
-    actual_output = trainable_engine%infer(inputs, matmul_t())
-    test_passes = [(abs(actual_output(i)%outputs() - expected_outputs(i)%outputs()) < tolerance, i=1, size(actual_output))]
-  end function
-
-  function train_on_and_truth_table_mini_batch() result(test_passes)
+  function train_and_gate() result(test_passes)
     logical, allocatable :: test_passes(:)
     type(mini_batch_t), allocatable :: mini_batches(:)
-    real(rkind), parameter :: false = 0._rkind, true = 1._rkind
+    type(inputs_t), allocatable :: training_inputs(:,:), tmp(:), test_inputs(:)
+    type(expected_outputs_t), allocatable :: training_outputs(:,:), expected_test_outputs(:), tmp2(:)
+    type(trainable_engine_t) trainable_engine
+    type(outputs_t), allocatable :: actual_outputs(:)
+    real(rkind), parameter :: tolerance = 1.E-02_rkind
+    real(rkind), allocatable :: harvest(:,:,:)
+    integer, parameter :: num_inputs=2, mini_batch_size = 1, num_iterations=14000
+    integer batch, iter, i
 
-    define_training_data: &
-    block
-      type(inputs_t), allocatable :: inputs(:,:), tmp(:)
-      type(expected_outputs_t), allocatable :: expected_outputs(:,:)
-      real(rkind), allocatable :: harvest(:,:,:)
-      integer, parameter :: num_inputs=2, mini_batch_size = 1, num_iterations=30000
-      integer batch, iter
+    call random_init(image_distinct=.true., repeatable=.true.)
+    allocate(harvest(num_inputs, mini_batch_size, num_iterations))
+    call random_number(harvest)
+    harvest = 2.*harvest - 1.
 
-      call random_init(image_distinct=.true., repeatable=.true.)
-      allocate(harvest(num_inputs, mini_batch_size, num_iterations))
-      call random_number(harvest)
+    ! The following temporary copies are required by gfortran bug 100650 and possibly 49324
+    ! See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100650 and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49324
+    tmp = [([(inputs_t(merge(true, false, harvest(:,batch,iter) < 0.5E0)), batch=1, mini_batch_size)], iter=1, num_iterations)]
+    training_inputs = reshape(tmp, [mini_batch_size, num_iterations])
 
-      ! The following temporary copy is required by gfortran bug 100650 and possibly 49324
-      ! See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100650 and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49324
-      tmp = [([(inputs_t(merge(true, false, harvest(:,batch,iter) < 0.5E0)), batch=1, mini_batch_size)], iter=1, num_iterations)]
-      inputs = reshape(tmp, [mini_batch_size, num_iterations])
-      expected_outputs = and(inputs)
-      mini_batches = [(mini_batch_t(input_output_pair_t(inputs(:,iter), expected_outputs(:,iter))), iter=1, num_iterations)]
-    end block define_training_data
-        
-    train_and_test: &
-    block
-      type(trainable_engine_t) trainable_engine
-      type(inputs_t), allocatable :: test_inputs(:)
-      type(outputs_t), allocatable :: actual_output(:)
-      type(expected_outputs_t), allocatable :: expected_test_outputs(:)
-      real(rkind), parameter :: tolerance = 1.E-02_rkind
-      integer i
+    tmp2 = [([(and(training_inputs(batch, iter)), batch = 1, mini_batch_size)], iter = 1, num_iterations )]
+    training_outputs = reshape(tmp2, [mini_batch_size, num_iterations])
 
-      trainable_engine = two_random_hidden_layers()
-      call trainable_engine%train(mini_batches)
-      test_inputs = [inputs_t([true,true]), inputs_t([false,true]), inputs_t([true,false]), inputs_t([false,false])]
-      expected_test_outputs = and(test_inputs)
-      actual_output = trainable_engine%infer_from_inputs_object_(test_inputs)
-      test_passes = [(abs(actual_output(i)%outputs() - expected_test_outputs(i)%outputs()) < tolerance, i=1, size(actual_output))]
-    end block train_and_test
+    mini_batches = [(mini_batch_t(input_output_pair_t(training_inputs(:,iter), training_outputs(:,iter))), iter=1, num_iterations)]        
+    trainable_engine = two_random_hidden_layers()
+
+    call trainable_engine%train(mini_batches)
+
+    test_inputs = [inputs_t([true,true]), inputs_t([false,true]), inputs_t([true,false]), inputs_t([false,false])]
+    expected_test_outputs = [(and(test_inputs(i)), i=1, size(test_inputs))]
+    actual_outputs = trainable_engine%infer_from_inputs_object_(test_inputs)
+    test_passes = [(abs(actual_outputs(i)%outputs() - expected_test_outputs(i)%outputs()) < tolerance, i=1, size(actual_outputs))]
+
+  contains
+
+    elemental function and(inputs_object) result(expected_outputs_object)
+      type(inputs_t), intent(in) :: inputs_object 
+      type(expected_outputs_t) expected_outputs_object 
+      expected_outputs_object = expected_outputs_t([merge(true, false, sum(inputs_object%values()) > 1.99_rkind)])
+    end function
+
+  end function
+
+  function train_not_and_gate() result(test_passes)
+    logical, allocatable :: test_passes(:)
+    type(mini_batch_t), allocatable :: mini_batches(:)
+    type(inputs_t), allocatable :: training_inputs(:,:), tmp(:), test_inputs(:)
+    type(expected_outputs_t), allocatable :: training_outputs(:,:), expected_test_outputs(:), tmp2(:)
+    type(trainable_engine_t) trainable_engine
+    type(outputs_t), allocatable :: actual_outputs(:)
+    real(rkind), parameter :: tolerance = 1.E-02_rkind
+    real(rkind), allocatable :: harvest(:,:,:)
+    integer, parameter :: num_inputs=2, mini_batch_size = 1, num_iterations=14000
+    integer batch, iter, i
+
+    call random_init(image_distinct=.true., repeatable=.true.)
+    allocate(harvest(num_inputs, mini_batch_size, num_iterations))
+    call random_number(harvest)
+    harvest = 2.*harvest - 1.
+
+    ! The following temporary copies are required by gfortran bug 100650 and possibly 49324
+    ! See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100650 and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49324
+    tmp = [([(inputs_t(merge(true, false, harvest(:,batch,iter) < 0.5E0)), batch=1, mini_batch_size)], iter=1, num_iterations)]
+    training_inputs = reshape(tmp, [mini_batch_size, num_iterations])
+
+    tmp2 = [([(not_and(training_inputs(batch, iter)), batch = 1, mini_batch_size)], iter = 1, num_iterations )]
+    training_outputs = reshape(tmp2, [mini_batch_size, num_iterations])
+
+    mini_batches = [(mini_batch_t(input_output_pair_t(training_inputs(:,iter), training_outputs(:,iter))), iter=1, num_iterations)]        
+    trainable_engine = two_random_hidden_layers()
+
+    call trainable_engine%train(mini_batches)
+
+    test_inputs = [inputs_t([true,true]), inputs_t([false,true]), inputs_t([true,false]), inputs_t([false,false])]
+    expected_test_outputs = [(not_and(test_inputs(i)), i=1, size(test_inputs))]
+    actual_outputs = trainable_engine%infer_from_inputs_object_(test_inputs)
+    test_passes = [(abs(actual_outputs(i)%outputs() - expected_test_outputs(i)%outputs()) < tolerance, i=1, size(actual_outputs))]
 
   contains
     
-    elemental function and(inputs_object) result(expected_outputs_object)
-       type(inputs_t), intent(in) :: inputs_object 
-       type(expected_outputs_t) expected_outputs_object 
-       expected_outputs_object = expected_outputs_t([merge(false, true, sum(inputs_object%values())<=1.5_rkind)])
+    function not_and(inputs) result(expected_outputs)
+       type(inputs_t), intent(in) :: inputs
+       type(expected_outputs_t) expected_outputs
+       expected_outputs = expected_outputs_t([merge(true, false, sum(inputs%values()) < 2.)])
     end function
 
-    function two_random_hidden_layers() result(trainable_engine)
-      type(trainable_engine_t) trainable_engine
-      integer, parameter :: inputs = 2, outputs = 1, hidden = 3 ! number of neurons in input, output, and hidden layers
-      integer, parameter :: neurons(*) = [inputs, hidden, hidden, outputs] ! neurons per layer
-      integer, parameter :: max_neurons = maxval(neurons), layers=size(neurons) ! max layer width, number of layers
-      real(rkind) w(max_neurons, max_neurons, layers-1), b(max_neurons, max_neurons)
-
-      call random_number(w)
-      call random_number(b)
-     
-      trainable_engine = trainable_engine_t( &
-        nodes = neurons, weights = w, biases = b, differentiable_activation_strategy = sigmoid_t(), metadata = &
-        [string_t("2-hide|3-wide"), string_t("Damian Rouson"), string_t("2023-06-28"), string_t("sigmoid"), string_t("false")] &
-      )   
-    end function
   end function
 
 end module trainable_engine_test_m
