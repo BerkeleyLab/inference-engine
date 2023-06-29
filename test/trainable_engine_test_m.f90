77d04f261f1466823f3ec669b0ea48e366784f67
Damian Rouson <rouson@lbl.gov>
2023-06-29 20:41:05 +0200

    test(train): OR test, print_truth_table subroutine

--- test/trainable_engine_test_m.f90
+++ test/trainable_engine_test_m.f90
@@ -28,6 +28,16 @@ module trainable_engine_test_m
 
   real(rkind), parameter :: false = 0._rkind, true = 1._rkind
 
+  abstract interface
+
+    function map_i(inputs) result(expected_outputs)
+      import inputs_t, expected_outputs_t
+      type(inputs_t), intent(in) :: inputs
+      type(expected_outputs_t) expected_outputs
+    end function
+
+  end interface
+
 contains
 
   pure function subject() result(specimen)
@@ -39,28 +49,52 @@ contains
     type(test_result_t), allocatable :: test_results(:)
 
     character(len=*), parameter :: longest_description = &
-        "learning the mapping (false,false) -> false with 2 hidden layers trained on AND-gate input/output pairs"
+        "learning the mapping (false,false) -> false with 2 hidden layers trained with symmetric data representing an OR gate" 
 
     associate( &
       descriptions => &
       [ character(len=len(longest_description)) :: &
-        "learning the mapping (true,true) -> true with 2 hidden layers trained on AND-gate input/output pairs", &
-        "learning the mapping (false,true) -> false with 2 hidden layers trained on AND-gate input/output pairs", &
-        "learning the mapping (true,false) -> false with 2 hidden layers trained on AND-gate input/output pairs", &
-        "learning the mapping (false,false) -> false with 2 hidden layers trained on AND-gate input/output pairs", &
-        "learning the mapping (true,true) -> false with 2 hidden layers trained on NOT_AND-gate input/output pairs", &
-        "learning the mapping (false,true) -> true with 2 hidden layers trained on NOT-AND-gate input/output pairs", &
-        "learning the mapping (true,false) -> true with 2 hidden layers trained on NOT-AND-gate input/output pairs", &
-        "learning the mapping (false,false) -> true with 2 hidden layers trained on NOT-AND-gate input/output pairs" &
+        "learning the mapping (true,true) -> true with 2 hidden layers trained with skewed data representing an AND gate"    , &
+        "learning the mapping (false,true) -> false with 2 hidden layers trained with skewed data representing an AND gate"  , &
+        "learning the mapping (true,false) -> false with 2 hidden layers trained with skewed data representing an AND gate"  , &
+        "learning the mapping (false,false) -> false with 2 hidden layers trained with skewed data representing anAND gate"  , &
+        "learning the mapping (true,true) -> false with 2 hidden layers trained with skewed data representing NOT-AND logic" , &
+        "learning the mapping (false,true) -> true with 2 hidden layers trained with skewed data representing NOT-AND logic" , &
+        "learning the mapping (true,false) -> true with 2 hidden layers trained with skewed data representing NOT-AND logic" , &
+        "learning the mapping (false,false) -> true with 2 hidden layers trained with skewed data representing NOT-AND logic", &
+        "learning the mapping (true,true) -> true with 2 hidden layers trained with symmetric data representing an OR gate"  , &
+        "learning the mapping (false,true) -> true with 2 hidden layers trained with symmetric data representing an OR gate,", &
+        "learning the mapping (true,false) -> true with 2 hidden layers trained with symmetric data representing an OR gate", &
+        "learning the mapping (false,false) -> false with 2 hidden layers trained with symmetric data representing an OR gate" &
       ], outcomes => [ &
-        train_and_gate(), &
-        train_not_and_gate() &
+        and_gate_with_skewed_training_data(), &
+        not_and_gate_with_skewed_training_data(), &
+        or_gate_with_symmetric_training_data() &
       ] &
     )
       call assert(size(descriptions) == size(outcomes), "trainable_engine_test_m(results): size(descriptions) == size(outcomes)")
       test_results = test_result_t(descriptions, outcomes)
     end associate
   end function
+
+  subroutine print_truth_table(gate_name, gate_function_ptr, test_inputs, actual_outputs)
+    character(len=*), intent(in) :: gate_name
+    procedure(map_i), intent(in), pointer :: gate_function_ptr
+    type(inputs_t), intent(in) :: test_inputs(:)
+    type(outputs_t), intent(in) :: actual_outputs(:)
+    type(expected_outputs_t) expected_outputs
+    integer i
+
+    call assert( size(test_inputs) == size(actual_outputs), &
+      "trainable_engine_test_m(print_truth_table): size(test_inputs) == size(actual_outputs)")
+
+    print *,"_______" // gate_name // "_______"
+
+    do i = 1, size(test_inputs)
+      expected_outputs = gate_function_ptr(test_inputs(i))
+      print *,test_inputs(i)%values(), "-->", expected_outputs%outputs(), ":", actual_outputs(i)%outputs()
+    end do
+  end subroutine
 
   function two_random_hidden_layers() result(trainable_engine)
     type(trainable_engine_t) trainable_engine
@@ -78,7 +112,7 @@ contains
     )   
   end function
 
-  function train_and_gate() result(test_passes)
+  function and_gate_with_skewed_training_data() result(test_passes)
     logical, allocatable :: test_passes(:)
     type(mini_batch_t), allocatable :: mini_batches(:)
     type(inputs_t), allocatable :: training_inputs(:,:), tmp(:), test_inputs(:)
@@ -123,7 +157,7 @@ contains
 
   end function
 
-  function train_not_and_gate() result(test_passes)
+  function not_and_gate_with_skewed_training_data() result(test_passes)
     logical, allocatable :: test_passes(:)
     type(mini_batch_t), allocatable :: mini_batches(:)
     type(inputs_t), allocatable :: training_inputs(:,:), tmp(:), test_inputs(:)
@@ -164,6 +198,50 @@ contains
        type(inputs_t), intent(in) :: inputs
        type(expected_outputs_t) expected_outputs
        expected_outputs = expected_outputs_t([merge(true, false, sum(inputs%values()) < 2.)])
+    end function
+
+  end function
+
+  function or_gate_with_symmetric_training_data() result(test_passes)
+    logical, allocatable :: test_passes(:)
+    type(mini_batch_t), allocatable :: mini_batches(:)
+    type(inputs_t), allocatable :: training_inputs(:,:), tmp(:), test_inputs(:)
+    type(expected_outputs_t), allocatable :: training_outputs(:,:), expected_test_outputs(:), tmp2(:)
+    type(trainable_engine_t) trainable_engine
+    type(outputs_t), allocatable :: actual_outputs(:)
+    real(rkind), parameter :: tolerance = 1.E-02_rkind
+    real(rkind), allocatable :: harvest(:,:,:)
+    integer, parameter :: num_inputs=2, mini_batch_size = 1, num_iterations=26000
+    integer batch, iter, i
+
+    call random_init(image_distinct=.true., repeatable=.true.)
+    allocate(harvest(num_inputs, mini_batch_size, num_iterations))
+    call random_number(harvest)
+
+    ! The following temporary copies are required by gfortran bug 100650 and possibly 49324
+    ! See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100650 and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49324
+    tmp = [([(inputs_t(merge(true, false, harvest(:,batch,iter) < 0.5E0)), batch=1, mini_batch_size)], iter=1, num_iterations)]
+    training_inputs = reshape(tmp, [mini_batch_size, num_iterations])
+
+    tmp2 = [([(or(training_inputs(batch, iter)), batch = 1, mini_batch_size)], iter = 1, num_iterations )]
+    training_outputs = reshape(tmp2, [mini_batch_size, num_iterations])
+
+    mini_batches = [(mini_batch_t(input_output_pair_t(training_inputs(:,iter), training_outputs(:,iter))), iter=1, num_iterations)]        
+    trainable_engine = two_random_hidden_layers()
+
+    call trainable_engine%train(mini_batches)
+
+    test_inputs = [inputs_t([true,true]), inputs_t([false,true]), inputs_t([true,false]), inputs_t([false,false])]
+    expected_test_outputs = [(or(test_inputs(i)), i=1, size(test_inputs))]
+    actual_outputs = trainable_engine%infer_from_inputs_object_(test_inputs)
+    test_passes = [(abs(actual_outputs(i)%outputs() - expected_test_outputs(i)%outputs()) < tolerance, i=1, size(actual_outputs))]
+
+  contains
+    
+    function or(inputs) result(expected_outputs)
+       type(inputs_t), intent(in) :: inputs
+       type(expected_outputs_t) expected_outputs
+       expected_outputs = expected_outputs_t([merge(true, false, sum(inputs%values()) > 0.99)])
     end function
 
   end function
