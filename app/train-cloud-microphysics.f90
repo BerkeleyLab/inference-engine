712a1577a4a9a90d9620a0d240db8232afe6b128
Damian Rouson <rouson@lbl.gov>
2023-08-25 21:03:31 -0700

    feat(microphysics): 1st complete draft of training

--- app/train-cloud-microphysics.f90
+++ app/train-cloud-microphysics.f90
@@ -33,6 +33,7 @@ program train_cloud_microphysics
   !! External dependencies:
   use sourcery_m, only : string_t, file_t, command_line_t
   use assert_m, only : assert, intrinsic_array_t
+  use ieee_arithmetic, only : ieee_is_nan
 
   !! Internal dependencies;
   use inference_engine_m, only : &
@@ -56,18 +57,25 @@ program train_cloud_microphysics
     read_and_train: &
     block
       real, allocatable, dimension(:,:,:,:) :: &
-        pressure_in, potential_temperature_in, temperature_in, qv_in, qc_in, qi_in, qr_in, qs_in, &
-        pressure_out, potential_temperature_out, temperature_out, qv_out, qc_out, qi_out, qr_out, qs_out
-      double precision, allocatable, dimension(:) :: time_in, time_out
-      integer, allocatable :: lbounds(:)
+        pressure_in , potential_temperature_in , temperature_in , &
+        pressure_out, potential_temperature_out, temperature_out, &
+        qv_out, qc_out, qi_out, qr_out, qs_out, &
+        qv_in , qc_in , qi_in , qr_in , qs_in , &
+        dpt_dt, dqv_dt, dqc_dt, dqi_dt, dqr_dt, dqs_dt
       type(ubounds_t), allocatable :: ubounds(:)
+      double precision, allocatable, dimension(:) :: time_in, time_out
       double precision, parameter :: tolerance = 1.E-07
+      integer, allocatable :: lbounds(:)
       integer, parameter :: initial = 1
+      integer t_end, t
+ 
+      print *,"Starting to read network inputs from " // network_input
 
       associate(network_input_file => netCDF_file_t(network_input))
         ! Skipping the following unnecessary inputs that are in the current file format as of 14 Aug 2023:
         ! precipitation, snowfall
         call network_input_file%input("pressure", pressure_in)
+
         call network_input_file%input("potential_temperature", potential_temperature_in)
         call network_input_file%input("temperature", temperature_in)
         call network_input_file%input("qv", qv_in)
@@ -76,12 +84,16 @@ program train_cloud_microphysics
         call network_input_file%input("qr", qr_in)
         call network_input_file%input("qs", qs_in)
         call network_input_file%input("time", time_in)
+        t_end = size(time_in)
         lbounds = &
           [lbound(pressure_in), lbound(temperature_in), lbound(qv_in), lbound(qc_in), lbound(qi_in), lbound(qr_in), lbound(qs_in)]
         ubounds = &
-          [ubounds_t(ubound(pressure_in)), ubounds_t(ubound(temperature_in)), ubounds_t(ubound(qv_in)), ubounds_t(ubound(qc_in)),&
-           ubounds_t(ubound(qi_in)), ubounds_t(ubound(qr_in)), ubounds_t(ubound(qs_in))]
+          [ubounds_t(ubound(qv_in)), ubounds_t(ubound(qc_in)), ubounds_t(ubound(qi_in)), &
+           ubounds_t(ubound(qr_in)), ubounds_t(ubound(qs_in)), ubounds_t(ubound(pressure_in)), ubounds_t(ubound(temperature_in)) &
+          ]
       end associate
+
+      print *,"Starting to read network outputs from " // network_output
 
       associate(network_output_file => netCDF_file_t(network_output))
         call network_output_file%input("potential_temperature", potential_temperature_out)
@@ -98,82 +110,94 @@ program train_cloud_microphysics
           ubounds_t(ubound(qr_out)), ubounds_t(ubound(qs_out))]
         call assert(all(lbounds == 1), "main: default input/output lower bounds", intrinsic_array_t(lbounds))
         call assert(all(ubounds == ubounds(1)), "main: matching input/output upper bounds")
-        call assert(all(abs(time_in - time_out)<tolerance), "main: matching time stamps")
+        call assert(all(abs(time_in(2:t_end) - time_out(1:t_end-1))<tolerance), "main: matching time stamps")
       end associate
 
-      print *,"time stamps:"
-      print *,time_in
+      print *,"Estimating time derivatives"
+  
+      allocate(dpt_dt, mold = potential_temperature_out)
+      allocate(dqv_dt, mold = qv_out)
+      allocate(dqc_dt, mold = qc_out)
+      allocate(dqi_dt, mold = qi_out)
+      allocate(dqr_dt, mold = qr_out)
+      allocate(dqs_dt, mold = qs_out)
 
-      associate(dt => real(time_in(initial+1) - time_in(initial), rkind))
-        associate( &
-          dpt_dt => (potential_temperature_out - potential_temperature_in)/dt, &
-          dqv_dt => (qv_out - qv_in)/dt, &
-          dqc_dt => (qc_out - qc_in)/dt, &
-          dqi_dt => (qi_out - qi_in)/dt, &
-          dqr_dt => (qr_out - qr_in)/dt, &
-          dqs_dt => (qs_out - qs_in)/dt  &
-        )
-        train_network: &
-          block
-            ! As a first test, let's check whether we can train based on a tiny subset of the training data.
-            ! For this purposes, we somewhat arbitrarily gather input/output pairs along a line of constsant 
-            ! longitude and mid-level elevation at the final time step
-            type(trainable_engine_t) trainable_engine
-            type(inference_engine_t) inference_engine
-            type(mini_batch_t), allocatable :: mini_batches(:)
-            type(tensor_t), allocatable, dimension(:,:) :: inputs, outputs
-            type(tensor_t), allocatable, dimension(:) :: tmp1, tmp2
-            real(rkind) t_start, t_end
-            integer, parameter :: mini_batch_size=1
-            integer batch, lon, num_in, num_out
-            type(file_t) json_file
+      associate(dt => real(time_out - time_in, rkind))
+        do concurrent(t = 1:t_end)
+          dpt_dt(:,:,:,t) = (potential_temperature_out(:,:,:,t) - potential_temperature_in(:,:,:,t))/dt(t)
+          dqv_dt(:,:,:,t) = (qv_out(:,:,:,t)- qv_in(:,:,:,t))/dt(t)
+          dqc_dt(:,:,:,t) = (qc_out(:,:,:,t)- qc_in(:,:,:,t))/dt(t)
+          dqi_dt(:,:,:,t) = (qi_out(:,:,:,t)- qi_in(:,:,:,t))/dt(t)
+          dqr_dt(:,:,:,t) = (qr_out(:,:,:,t)- qr_in(:,:,:,t))/dt(t)
+          dqs_dt(:,:,:,t) = (qs_out(:,:,:,t)- qs_in(:,:,:,t))/dt(t)
+        end do
+      end associate
 
-            associate(t => ubound(qc_in,1), level => ubound(qc_in,2)/2, lat => ubound(qc_in,3)/2, num_batches => ubound(qc_in,4))
+      call assert(.not. any(ieee_is_nan(dpt_dt)), ".not. any(ieee_is_nan(dpt_dt)")
+      call assert(.not. any(ieee_is_nan(dqv_dt)), ".not. any(ieee_is_nan(dqv_dt)")
+      call assert(.not. any(ieee_is_nan(dqc_dt)), ".not. any(ieee_is_nan(dqc_dt)")
+      call assert(.not. any(ieee_is_nan(dqi_dt)), ".not. any(ieee_is_nan(dqi_dt)")
+      call assert(.not. any(ieee_is_nan(dqr_dt)), ".not. any(ieee_is_nan(dqr_dt)")
+      call assert(.not. any(ieee_is_nan(dqs_dt)), ".not. any(ieee_is_nan(dqs_dt)")
 
-              ! The following temporary copies are required by gfortran bug 100650 and possibly 49324
-              ! See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100650 and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49324
-              tmp1 = [( &
-                tensor_t( &
-                  [pressure_in(t,level,lat,lon), potential_temperature_in(t,level,lat,lon), temperature_in(t,level,lat,lon), &
-                   qv_in(t,level,lat,lon), qc_in(t,level,lat,lon), qi_in(t,level,lat,lon), qr_in(t,level,lat,lon), &
-                   qs_in(t,level,lat,lon) &
-                  ] &
-                ), lon = 1, num_batches)]
-              inputs = reshape(tmp1, [mini_batch_size, num_batches])
+      train_network: &
+      block
+        ! As a first test, let's check whether we can train based on a tiny subset of the training data.
+        ! For this purposes, we somewhat arbitrarily gather input/output pairs along a line of constsant 
+        ! longitude and mid-level elevation at the final time step
+        type(trainable_engine_t) trainable_engine
+        type(inference_engine_t) inference_engine
+        type(mini_batch_t), allocatable :: mini_batches(:)
+        type(tensor_t), allocatable, dimension(:,:) :: inputs, outputs
+        type(tensor_t), allocatable, dimension(:) :: tmp1, tmp2
+        real(rkind) t_start, t_end
+        integer, parameter :: mini_batch_size=1
+        integer batch, lon, num_in, num_out
+        type(file_t) json_file
 
-              tmp2 = [( &
-                tensor_t( &
-                  [dpt_dt(t,level,lat,lon), dqv_dt(t,level,lat,lon), dqc_dt(t,level,lat,lon), &
-                   dqi_dt(t,level,lat,lon), dqr_dt(t,level,lat,lon), dqs_dt(t,level,lat,lon) &
-                  ] &
-                ), lon = 1, num_batches)]
-              outputs = reshape(tmp2, [mini_batch_size, num_batches])
+        associate(num_mini_batches => size(qc_in,1), lat => ubound(qc_in,2)/2, level => ubound(qc_in,3)/2, time=> ubound(qc_in,4))
 
-              mini_batches = [(mini_batch_t(input_output_pair_t(inputs(:,batch), outputs(:,batch))), batch = 1, num_batches)]
+          print *,"Defining tensors"
 
-              num_in = inputs(1,1)%num_components()
-              num_out = outputs(1,1)%num_components()
+          ! The following temporary copies are required by gfortran bug 100650 and possibly 49324
+          ! See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100650 and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49324
+          tmp1 = [( &
+            tensor_t( &
+              [pressure_in(lon,lat,level,time), potential_temperature_in(lon,lat,level,time), temperature_in(lon,lat,level,time),&
+               qv_in(lon,lat,level,time), qc_in(lon,lat,level,time), qi_in(lon,lat,level,time), qr_in(lon,lat,level,time), &
+               qs_in(lon,lat,level,time) &
+              ] &
+            ), lon = 1, num_mini_batches)]
+          inputs = reshape(tmp1, [mini_batch_size, num_mini_batches])
 
-              trainable_engine = random_hidden_layers( &
-                num_inputs = inputs(1,1)%num_components(), num_outputs = outputs(1,1)%num_components() &
-              )
-              call cpu_time(t_start)
-              call trainable_engine%train(mini_batches)
-              call cpu_time(t_end)
+          tmp2 = [( &
+            tensor_t( &
+              [dpt_dt(lon,lat,level,time), dqv_dt(lon,lat,level,time), dqc_dt(lon,lat,level,time), &
+               dqi_dt(lon,lat,level,time), dqr_dt(lon,lat,level,time), dqs_dt(lon,lat,level,time) &
+              ] &
+            ), lon = 1, num_mini_batches)]
+          outputs = reshape(tmp2, [mini_batch_size, num_mini_batches])
 
-              print *, "Training time: ", t_end - t_start
- 
-              inference_engine = trainable_engine%to_inference_engine()
-              json_file = inference_engine%to_json()
-              call json_file%write_lines(string_t(network_file))
-            end associate
-
-          end block train_network
+          mini_batches = [(mini_batch_t(input_output_pair_t(inputs(:,batch), outputs(:,batch))), batch = 1, num_mini_batches)]
         end associate
-      end associate
 
+        print *,"Training network"
+
+        trainable_engine = random_hidden_layers( &
+          num_inputs = inputs(1,1)%num_components(), num_outputs = outputs(1,1)%num_components() &
+        )
+        call cpu_time(t_start)
+        call trainable_engine%train(mini_batches)
+        call cpu_time(t_end)
+
+        print *, "Training time: ", t_end - t_start
+ 
+        inference_engine = trainable_engine%to_inference_engine()
+        json_file = inference_engine%to_json()
+        call json_file%write_lines(string_t(network_file))
+
+      end block train_network
     end block read_and_train
-
   end associate
 
   print *,new_line('a') // "______training_cloud_microhpysics done _______"
