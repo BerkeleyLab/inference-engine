0c294c9f36fe4205682d44bfb9b5fbbd85007654
Damian Rouson <rouson@lbl.gov>
2023-09-06 18:20:56 -0700

    feat(example): shorter compilation time

--- example/train-and-write.f90
+++ example/train-and-write.f90
@@ -18,75 +18,74 @@ program train_and_write
       'Usage: ./build/run-fpm.sh run --example train-and-write -- --output-file "<file-name>"' 
   end if
 
+  and_gate_with_skewed_training_data: &
   block
+    logical, allocatable :: test_passes(:)
+    type(mini_batch_t), allocatable :: mini_batches(:)
+    type(tensor_t), allocatable, dimension(:,:) :: training_inputs, training_outputs
+    type(tensor_t), allocatable, dimension(:) :: tmp, tmp2, test_inputs, expected_test_outputs, actual_outputs
     type(trainable_engine_t) trainable_engine
     type(inference_engine_t) inference_engine
-    type(file_t) json_file
-    type(mini_batch_t), allocatable :: mini_batches(:)
-    type(tensor_t), allocatable :: training_inputs(:,:), tmp(:), inputs(:)
-    type(tensor_t), allocatable :: training_outputs(:,:), tmp2(:), expected_outputs(:)
-    real(rkind) t_start, t_end
+    real(rkind), parameter :: tolerance = 1.E-02_rkind
     real(rkind), allocatable :: harvest(:,:,:)
-    integer, parameter :: num_inputs=2, mini_batch_size = 1, num_iterations=8000000
+    integer, parameter :: num_inputs=2, mini_batch_size = 1, num_iterations=20000
     integer batch, iter, i
+    type(file_t) json_file
 
     allocate(harvest(num_inputs, mini_batch_size, num_iterations))
     call random_number(harvest)
+    harvest = 2.*(harvest - 0.5) ! skew toward more input values being true
 
     ! The following temporary copies are required by gfortran bug 100650 and possibly 49324
     ! See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100650 and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49324
     tmp = [([(tensor_t(merge(true, false, harvest(:,batch,iter) < 0.5E0)), batch=1, mini_batch_size)], iter=1, num_iterations)]
     training_inputs = reshape(tmp, [mini_batch_size, num_iterations])
 
-    tmp2 = [([(xor(training_inputs(batch, iter)), batch = 1, mini_batch_size)], iter = 1, num_iterations )]
+    tmp2 = [([(and(training_inputs(batch, iter)), batch = 1, mini_batch_size)], iter = 1, num_iterations )]
     training_outputs = reshape(tmp2, [mini_batch_size, num_iterations])
 
     mini_batches = [(mini_batch_t(input_output_pair_t(training_inputs(:,iter), training_outputs(:,iter))), iter=1, num_iterations)]    
-    trainable_engine = one_random_hidden_layer()
+    trainable_engine = two_zeroed_hidden_layers()
 
-    call cpu_time(t_start)
-    call trainable_engine%train(mini_batches)
-    call cpu_time(t_end)
+    call trainable_engine%train(mini_batches,adam=.true.)
 
-    print *,"Training time: ",t_end - t_start
+    test_inputs = [tensor_t([true,true]), tensor_t([false,true]), tensor_t([true,false]), tensor_t([false,false])]
+    expected_test_outputs = [(and(test_inputs(i)), i=1, size(test_inputs))]
+    actual_outputs = trainable_engine%infer(test_inputs)
 
-    inputs = [tensor_t([true,true]), tensor_t([true,false]), tensor_t([false,true]), tensor_t([false,false])]
-    print *, "sample inputs:    ",("[",inputs(i)%values(),"]", i=1, size(inputs))
-    expected_outputs = xor(inputs)
-    print *, "expected outputs: ",(expected_outputs(i)%values(), i=1, size(expected_outputs))
-    associate(outputs => trainable_engine%infer(inputs))
-      print *, "actual outputs:   ",(outputs(i)%values(), i=1, size(outputs))
-    end associate
+    print *,"  Input 1          Input 2         Expected output   Actual output"
+    do i = 1, size(test_inputs)
+       print *, test_inputs(i)%values(), actual_outputs(i)%values(), expected_test_outputs(i)%values()
+    end do
 
-     inference_engine = trainable_engine%to_inference_engine()
-     json_file = inference_engine%to_json()
-     call json_file%write_lines(file_name)
-  end block
+    inference_engine = trainable_engine%to_inference_engine()
+    json_file = inference_engine%to_json()
+    call json_file%write_lines(file_name)
+
+  end block and_gate_with_skewed_training_data
 
 contains
 
-  elemental function xor(inputs) result(expected_outputs)
-    type(tensor_t), intent(in) :: inputs
-    type(tensor_t) expected_outputs
-    associate(sum_inputs => sum(inputs%values()))
-     expected_outputs = tensor_t([merge(true, false, sum_inputs > 0.99 .and. sum_inputs < 1.01)])
-    end associate
+  elemental function and(inputs_object) result(expected_outputs_object)
+    type(tensor_t), intent(in) :: inputs_object 
+    type(tensor_t) expected_outputs_object 
+    expected_outputs_object = tensor_t([merge(true, false, sum(inputs_object%values()) > 1.99_rkind)])
   end function
 
-  function one_random_hidden_layer() result(trainable_engine)
+  function two_zeroed_hidden_layers() result(trainable_engine)
     type(trainable_engine_t) trainable_engine
-    integer, parameter :: inputs = 2, outputs = 1, hidden = 2 ! number of neurons in input, output, and hidden layers
-    integer, parameter :: n(*) = [inputs, hidden, outputs] ! neurons per layer
-    integer, parameter :: n_max = maxval(n), layers=size(n) ! max layer width, number of layers
-    real(rkind) w(n_max, n_max, layers-1), b(n_max, n_max)
+    integer, parameter :: inputs = 2, outputs = 1, hidden = 3 ! number of neurons in input, output, and hidden layers
+    integer, parameter :: neurons(*) = [inputs, hidden, hidden, outputs] ! neurons per layer
+    integer, parameter :: max_neurons = maxval(neurons), layers=size(neurons) ! max layer width, number of layers
+    real(rkind) w(max_neurons, max_neurons, layers-1), b(max_neurons, max_neurons)
 
-    call random_number(b)
-    call random_number(w)
+    w = 0.
+    b = 0.
 
     trainable_engine = trainable_engine_t( &
-      nodes = n, weights = w, biases = b, differentiable_activation_strategy = sigmoid_t(), & 
-      metadata = [string_t("1 hide|2 wide"), string_t("D. Rouson"), string_t("2023-06-30"), string_t("sigmoid"), string_t("false")]&
-    ) 
+      nodes = neurons, weights = w, biases = b, differentiable_activation_strategy = sigmoid_t(), metadata = & 
+      [string_t("2-hide|3-wide"), string_t("Damian Rouson"), string_t("2023-06-30"), string_t("sigmoid"), string_t("false")] &
+    )   
   end function
 
 end program
