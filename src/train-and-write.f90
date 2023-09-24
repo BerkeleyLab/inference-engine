2d00af2c36f540b062b3a87bc94095e64d79da46
rouson <rouson@lbl.gov>
2023-09-24 00:16:57 +0000

    Deploying to gh-pages from @ BerkeleyLab/inference-engine@8b0f2b1fc53f535674aefef377ec98972b0e7b65 🚀

--- src/train-and-write.f90
+++ src/train-and-write.f90
@@ -1,91 +1,127 @@
 ! Copyright (c), The Regents of the University of California
 ! Terms of use are as specified in LICENSE.txt
 program train_and_write
-  !! This program demonstrates how to train a neural network and write it to a JSON file.
+  !! This program demonstrates how to train a simple neural network starting from a randomized initial condition and 
+  !! how to write the initial network and the trained network to separate JSON files.  The network has two hiden layers.
+  !! The input, hidden, and output layers are all two nodes wide.  The training data has outputs that identically match
+  !! the corresponding inputs.  Hence, the desired network represents an identity mapping.  With RELU activation functions,
+  !! the desired network therefore contains weights corresponding to identity matrices and biases that vanish everywhere.
+  !! The initial condition corresponds to the desired network with all weights and biases perturbed by a random variable
+  !! that is uniformly distributed on the range [0,0.1].
   use inference_engine_m, only : &
-    inference_engine_t, trainable_engine_t, rkind, sigmoid_t, mini_batch_t, tensor_t, input_output_pair_t
-  use sourcery_m, only : string_t, file_t, command_line_t
+    inference_engine_t, trainable_engine_t, mini_batch_t, tensor_t, input_output_pair_t, shuffle, relu_t
+  use sourcery_m, only : string_t, file_t, command_line_t, bin_t
+  use assert_m, only : assert, intrinsic_array_t
   implicit none
 
-  type(string_t) file_name
+  type(string_t) intial_network_file, final_network_file
   type(command_line_t) command_line
-  real(rkind), parameter :: false=0._rkind, true=1._rkind
 
-  file_name = string_t(command_line%flag_value("--output-file"))
+  final_network_file = string_t(command_line%flag_value("--output-file"))
 
-  if (len(file_name%string())==0) then
+  if (len(final_network_file%string())==0) then
     error stop new_line('a') // new_line('a') // &
       'Usage: ./build/run-fpm.sh run --example train-and-write -- --output-file "<file-name>"' 
   end if
 
-  and_gate_with_skewed_training_data: &
   block
-    logical, allocatable :: test_passes(:)
+    integer, parameter :: num_pairs = 5, num_epochs = 500, num_mini_batches= 3 ! num_pairs =  # input/output pairs in training data
+
     type(mini_batch_t), allocatable :: mini_batches(:)
-    type(tensor_t), allocatable, dimension(:,:) :: training_inputs, training_outputs
-    type(tensor_t), allocatable, dimension(:) :: tmp, tmp2, test_inputs, expected_test_outputs, actual_outputs
-    type(trainable_engine_t) trainable_engine
-    type(inference_engine_t) inference_engine
-    real(rkind), parameter :: tolerance = 1.E-02_rkind
-    real(rkind), allocatable :: harvest(:,:,:)
-    integer, parameter :: num_inputs=2, mini_batch_size = 1, num_iterations=20000
-    integer batch, iter, i
-    type(file_t) json_file
+    type(input_output_pair_t), allocatable :: input_output_pairs(:)
+    type(tensor_t), allocatable :: inputs(:)
+    type(trainable_engine_t)  trainable_engine
+    type(bin_t), allocatable :: bins(:)
+    real, allocatable :: cost(:), random_numbers(:)
 
-    allocate(harvest(num_inputs, mini_batch_size, num_iterations))
-    call random_number(harvest)
-    harvest = 2.*(harvest - 0.5) ! skew toward more input values being true
+    call random_init(image_distinct=.true., repeatable=.true.)
 
-    ! The following temporary copies are required by gfortran bug 100650 and possibly 49324
-    ! See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100650 and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49324
-    tmp = [([(tensor_t(merge(true, false, harvest(:,batch,iter) < 0.5E0)), batch=1, mini_batch_size)], iter=1, num_iterations)]
-    training_inputs = reshape(tmp, [mini_batch_size, num_iterations])
+    trainable_engine = perturbed_identity_network(perturbation_magnitude=0.2)
+    call output(trainable_engine%to_inference_engine(), string_t("initial-network.json"))
 
-    tmp2 = [([(and(training_inputs(batch, iter)), batch = 1, mini_batch_size)], iter = 1, num_iterations )]
-    training_outputs = reshape(tmp2, [mini_batch_size, num_iterations])
+    associate(num_inputs => trainable_engine%num_inputs(), num_outputs => trainable_engine%num_outputs())
 
-    mini_batches = [(mini_batch_t(input_output_pair_t(training_inputs(:,iter), training_outputs(:,iter))), iter=1, num_iterations)]    
-    trainable_engine = two_zeroed_hidden_layers()
+      call assert(num_inputs == num_outputs,"trainable_engine_test_m(identity_mapping): # inputs == # outputs", &
+        intrinsic_array_t([num_inputs, num_outputs]) &
+      )
+      block
+        integer i, j
+        inputs = [(tensor_t(real([(j*i, j = 1,num_inputs)])/(num_inputs*num_pairs)), i = 1, num_pairs)]
+      end block
+      associate(outputs => inputs)
+        input_output_pairs = input_output_pair_t(inputs, outputs)
+      end associate
+      block 
+        integer b
+        bins = [(bin_t(num_items=num_pairs, num_bins=num_mini_batches, bin_number=b), b = 1, num_mini_batches)]
+      end block
 
-    call trainable_engine%train(mini_batches,adam=.true.)
+      allocate(random_numbers(2:size(input_output_pairs)))
 
-    test_inputs = [tensor_t([true,true]), tensor_t([false,true]), tensor_t([true,false]), tensor_t([false,false])]
-    expected_test_outputs = [(and(test_inputs(i)), i=1, size(test_inputs))]
-    actual_outputs = trainable_engine%infer(test_inputs)
+      print *,"Cost"
+      block
+        integer e, b
+        do e = 1,num_epochs
+          call random_number(random_numbers)
+          call shuffle(input_output_pairs, random_numbers)
+          mini_batches = [(mini_batch_t(input_output_pairs(bins(b)%first():bins(b)%last())), b = 1, size(bins))]
+          call trainable_engine%train(mini_batches, cost, adam=.true.)
+          print *,sum(cost)/size(cost)
+        end do
+      end block
 
-    print *,"  Input 1          Input 2         Expected output   Actual output"
-    do i = 1, size(test_inputs)
-       print *, test_inputs(i)%values(), actual_outputs(i)%values(), expected_test_outputs(i)%values()
-    end do
+      block
+        real, parameter :: tolerance = 1.E-06
+        integer p
 
-    inference_engine = trainable_engine%to_inference_engine()
-    json_file = inference_engine%to_json()
-    call json_file%write_lines(file_name)
+        associate(network_outputs => trainable_engine%infer(inputs))
+          print *," Outputs                          |&
+                   Desired outputs                    |&
+                   Errors"
+          do p = 1, num_pairs
+            print *,network_outputs(p)%values(),"|", inputs(p)%values(), "|",  network_outputs(p)%values() - inputs(p)%values()
+          end do
+        end associate
+      end block
 
-  end block and_gate_with_skewed_training_data
+   end associate
+
+   call output(trainable_engine%to_inference_engine(), final_network_file)
+
+  end block
 
 contains
 
-  elemental function and(inputs_object) result(expected_outputs_object)
-    type(tensor_t), intent(in) :: inputs_object 
-    type(tensor_t) expected_outputs_object 
-    expected_outputs_object = tensor_t([merge(true, false, sum(inputs_object%values()) > 1.99_rkind)])
-  end function
+  subroutine output(inference_engine, file_name)
+    type(inference_engine_t), intent(in) :: inference_engine
+    type(string_t), intent(in) :: file_name
+    type(file_t) json_file
+    json_file = inference_engine%to_json()
+    call json_file%write_lines(file_name)
+  end subroutine
 
-  function two_zeroed_hidden_layers() result(trainable_engine)
+  function perturbed_identity_network(perturbation_magnitude) result(trainable_engine)
     type(trainable_engine_t) trainable_engine
-    integer, parameter :: inputs = 2, outputs = 1, hidden = 3 ! number of neurons in input, output, and hidden layers
-    integer, parameter :: neurons(*) = [inputs, hidden, hidden, outputs] ! neurons per layer
-    integer, parameter :: max_neurons = maxval(neurons), layers=size(neurons) ! max layer width, number of layers
-    real(rkind) w(max_neurons, max_neurons, layers-1), b(max_neurons, max_neurons)
+    real, intent(in) :: perturbation_magnitude
+    integer, parameter :: nodes_per_layer(*) = [2, 2, 2, 2]
+    integer, parameter :: max_n = maxval(nodes_per_layer), layers = size(nodes_per_layer)
+    integer i
+    real, parameter :: identity(*,*,*) = &
+      reshape(real([( [1,0], [0,1], i=1,layers-1 )]), [max_n, max_n, layers-1])
+    real w_harvest(size(identity,1), size(identity,2), size(identity,3)), b_harvest(size(identity,1), size(identity,3))
 
-    w = 0.
-    b = 0.
+    call random_number(w_harvest)
+    call random_number(b_harvest)
 
-    trainable_engine = trainable_engine_t( &
-      nodes = neurons, weights = w, biases = b, differentiable_activation_strategy = sigmoid_t(), metadata = & 
-      [string_t("2-hide|3-wide"), string_t("Damian Rouson"), string_t("2023-06-30"), string_t("sigmoid"), string_t("false")] &
-    )   
+    associate(w => identity + perturbation_magnitude*(w_harvest-0.5)/0.5, b => perturbation_magnitude*(b_harvest-0.5)/0.5)
+
+      trainable_engine = trainable_engine_t( &
+        nodes = nodes_per_layer, weights = w, biases = b, differentiable_activation_strategy = relu_t(), &
+        metadata = &
+          [string_t("Perturbed Identity"), string_t("Damian Rouson"), string_t("2023-09-23"), string_t("relu"), string_t("false")] &
+      )
+
+    end associate
   end function
 
 end program
