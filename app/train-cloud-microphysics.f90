ce3cb29efb4a84b2ebe020aa4a3019127c0b6bd3
Damian Rouson <rouson@lbl.gov>
2023-11-19 16:08:12 -0700

    refac(app):mv ubounds_m module to src subdirectory

--- app/train-cloud-microphysics.f90
+++ app/train-cloud-microphysics.f90
@@ -1,29 +1,8 @@
 ! Copyright (c), The Regents of the University of California
 ! Terms of use are as specified in LICENSE.txt
 #ifndef __INTEL_FORTRAN
-!! Due to a suspected bug in the Intel ifx compiler, the above C preprocessor macro
+!! Due to a suspected Intel ifx compilerlbug, the above C preprocessor macro
 !! effectively eliminates this file's source code when building with an Intel compiler.
-
-module ubounds_m
-  !! This module serves only to support array bounds checking in the main program below
-  implicit none
-
-  type ubounds_t
-    integer, allocatable :: ubounds_(:)
-  contains
-    procedure equals
-    generic :: operator(==) => equals
-  end type
- 
-contains
-
-  elemental function equals(lhs, rhs) result(lhs_equals_rhs)
-    class(ubounds_t), intent(in) :: lhs, rhs
-    logical lhs_equals_rhs
-    lhs_equals_rhs = all(lhs%ubounds_ == rhs%ubounds_)
-  end function
-
-end module
 
 program train_cloud_microphysics
   !! Train a neural network to represent the simplest cloud microphysics model from
