bf549a813157cf1328ddccbf80f8e2eaf066022f
Damian Rouson <rouson@lbl.gov>
2023-11-19 16:10:15 -0700

    refac(app): whitespace edits
    indent code inside associate construct in read_train_write
    subroutine

--- app/train-cloud-microphysics.f90
+++ app/train-cloud-microphysics.f90
@@ -144,171 +144,171 @@ contains
     )
       print *,"Reading network inputs from " // network_input
 
-    associate(network_input_file => netCDF_file_t(network_input))
-      ! Skipping the following unnecessary inputs that are in the current file format as of 14 Aug 2023:
-      ! precipitation, snowfall
-      call network_input_file%input("pressure", pressure_in)
-      call network_input_file%input("potential_temperature", potential_temperature_in)
-      call network_input_file%input("temperature", temperature_in)
-      call network_input_file%input("qv", qv_in)
-      call network_input_file%input("qc", qc_in)
-      call network_input_file%input("qi", qi_in)
-      call network_input_file%input("qr", qr_in)
-      call network_input_file%input("qs", qs_in)
-      call network_input_file%input("time", time_in)
-      t_end = size(time_in)
-      lbounds = &
-        [lbound(pressure_in), lbound(temperature_in), lbound(qv_in), lbound(qc_in), lbound(qi_in), lbound(qr_in), lbound(qs_in)]
-      ubounds = &
-        [ubounds_t(ubound(qv_in)), ubounds_t(ubound(qc_in)), ubounds_t(ubound(qi_in)), &
-         ubounds_t(ubound(qr_in)), ubounds_t(ubound(qs_in)), ubounds_t(ubound(pressure_in)), ubounds_t(ubound(temperature_in)) &
-        ]
-    end associate
-
-    print *,"Reading network outputs from " // network_output
-
-    associate(network_output_file => netCDF_file_t(network_output))
-      call network_output_file%input("potential_temperature", potential_temperature_out)
-      ! Skipping the following unnecessary outputs that are in the current file format as of 14 Aug 2023:
-      ! pressure, temperature, precipitation, snowfall
-      call network_output_file%input("qv", qv_out)
-      call network_output_file%input("qc", qc_out)
-      call network_output_file%input("qi", qi_out)
-      call network_output_file%input("qr", qr_out)
-      call network_output_file%input("qs", qs_out)
-      call network_output_file%input("time", time_out)
-      lbounds = [lbounds, lbound(qv_out), lbound(qc_out), lbound(qi_out), lbound(qr_out), lbound(qs_out)]
-      ubounds = [ubounds, ubounds_t(ubound(qv_out)), ubounds_t(ubound(qc_out)), ubounds_t(ubound(qi_out)), &
-        ubounds_t(ubound(qr_out)), ubounds_t(ubound(qs_out))]
-      call assert(all(lbounds == 1), "main: default input/output lower bounds", intrinsic_array_t(lbounds))
-      call assert(all(ubounds == ubounds(1)), "main: matching input/output upper bounds")
-      call assert(all(abs(time_in(2:t_end) - time_out(1:t_end-1))<tolerance), "main: matching time stamps")
-    end associate
-
-    print *,"Calculating time derivatives"
-  
-    allocate(dpt_dt, mold = potential_temperature_out)
-    allocate(dqv_dt, mold = qv_out)
-    allocate(dqc_dt, mold = qc_out)
-    allocate(dqi_dt, mold = qi_out)
-    allocate(dqr_dt, mold = qr_out)
-    allocate(dqs_dt, mold = qs_out)
-
-    associate(dt => real(time_out - time_in, rkind))
-      do concurrent(t = 1:t_end)
-        dpt_dt(:,:,:,t) = (potential_temperature_out(:,:,:,t) - potential_temperature_in(:,:,:,t))/dt(t)
-        dqv_dt(:,:,:,t) = (qv_out(:,:,:,t)- qv_in(:,:,:,t))/dt(t)
-        dqc_dt(:,:,:,t) = (qc_out(:,:,:,t)- qc_in(:,:,:,t))/dt(t)
-        dqi_dt(:,:,:,t) = (qi_out(:,:,:,t)- qi_in(:,:,:,t))/dt(t)
-        dqr_dt(:,:,:,t) = (qr_out(:,:,:,t)- qr_in(:,:,:,t))/dt(t)
-        dqs_dt(:,:,:,t) = (qs_out(:,:,:,t)- qs_in(:,:,:,t))/dt(t)
-      end do
-    end associate
-
-    call assert(.not. any(ieee_is_nan(dpt_dt)), ".not. any(ieee_is_nan(dpt_dt)")
-    call assert(.not. any(ieee_is_nan(dqv_dt)), ".not. any(ieee_is_nan(dqv_dt)")
-    call assert(.not. any(ieee_is_nan(dqc_dt)), ".not. any(ieee_is_nan(dqc_dt)")
-    call assert(.not. any(ieee_is_nan(dqi_dt)), ".not. any(ieee_is_nan(dqi_dt)")
-    call assert(.not. any(ieee_is_nan(dqr_dt)), ".not. any(ieee_is_nan(dqr_dt)")
-    call assert(.not. any(ieee_is_nan(dqs_dt)), ".not. any(ieee_is_nan(dqs_dt)")
-
-    train_network: &
-    block
-      type(trainable_engine_t) trainable_engine
-      type(mini_batch_t), allocatable :: mini_batches(:)
-      type(bin_t), allocatable :: bins(:)
-      type(input_output_pair_t), allocatable :: input_output_pairs(:)
-      type(tensor_t), allocatable, dimension(:) :: inputs, outputs
-      real(rkind), parameter :: keep = 0.01
-      real(rkind), allocatable :: cost(:)
-      real(rkind), allocatable :: harvest(:)
-      integer i, batch, lon, lat, level, time, network_unit, io_status, final_step, epoch
-
-      open(newunit=network_unit, file=network_file, form='formatted', status='old', iostat=io_status, action='read')
-
-      if (io_status==0) then
-        print *,"Reading network from file " // network_file
-        trainable_engine = trainable_engine_t(inference_engine_t(file_t(string_t(network_file))))
-        close(network_unit)
-      else
-        close(network_unit)
-        print *,"Initializing a new network"
-        trainable_engine = new_engine(training_configuration, randomize=.true.)
-      end if
-
-      if (.not. allocated(end_step)) end_step = t_end
-      
-      print *,"Defining tensors from time step", start_step, "through", end_step, "with strides of", stride
-
-      ! The following temporary copies are required by gfortran bug 100650 and possibly 49324
-      ! See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100650 and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49324
-      inputs = [( [( [( [( &
-        tensor_t( &
-        [ pressure_in(lon,lat,level,time), potential_temperature_in(lon,lat,level,time), temperature_in(lon,lat,level,time),&
-          qv_in(lon,lat,level,time), qc_in(lon,lat,level,time), qi_in(lon,lat,level,time), qr_in(lon,lat,level,time), &
-          qs_in(lon,lat,level,time) &
-        ] &
-        ), lon = 1, size(qv_in,1))], lat = 1, size(qv_in,2))], level = 1, size(qv_in,3))], time = start_step, end_step, stride)]
-
-      outputs = [( [( [( [( &
-        tensor_t( &
-          [dpt_dt(lon,lat,level,time), dqv_dt(lon,lat,level,time), dqc_dt(lon,lat,level,time), &
-           dqi_dt(lon,lat,level,time), dqr_dt(lon,lat,level,time), dqs_dt(lon,lat,level,time) &
-          ] &
-        ), lon = 1, size(qv_in,1))], lat = 1, size(qv_in,2))], level = 1, size(qv_in,3))], time = start_step, end_step, stride)]
-      
-      print *, "Eliminating",int(100*(1.-keep)),"% of the grid points that have all-zero time derivatives"
-
-      associate(num_grid_pts => size(outputs))
-        if (allocated(harvest)) deallocate(harvest)
-        allocate(harvest(num_grid_pts))
-        call random_number(harvest)
-        associate(keepers => [(any(outputs(i)%values()/=0.) .or. harvest(i)<keep, i=1,num_grid_pts)])
-          input_output_pairs = input_output_pair_t(pack(inputs, keepers), pack(outputs, keepers))
-          print *, size(input_output_pairs), "points retained out of ", num_grid_pts, " points total"
-        end associate
+      associate(network_input_file => netCDF_file_t(network_input))
+        ! Skipping the following unnecessary inputs that are in the current file format as of 14 Aug 2023:
+        ! precipitation, snowfall
+        call network_input_file%input("pressure", pressure_in)
+        call network_input_file%input("potential_temperature", potential_temperature_in)
+        call network_input_file%input("temperature", temperature_in)
+        call network_input_file%input("qv", qv_in)
+        call network_input_file%input("qc", qc_in)
+        call network_input_file%input("qi", qi_in)
+        call network_input_file%input("qr", qr_in)
+        call network_input_file%input("qs", qs_in)
+        call network_input_file%input("time", time_in)
+        t_end = size(time_in)
+        lbounds = &
+          [lbound(pressure_in), lbound(temperature_in), lbound(qv_in), lbound(qc_in), lbound(qi_in), lbound(qr_in), lbound(qs_in)]
+        ubounds = &
+          [ubounds_t(ubound(qv_in)), ubounds_t(ubound(qc_in)), ubounds_t(ubound(qi_in)), &
+           ubounds_t(ubound(qr_in)), ubounds_t(ubound(qs_in)), ubounds_t(ubound(pressure_in)), ubounds_t(ubound(temperature_in)) &
+          ]
       end associate
 
-      associate( &
-        num_pairs => size(input_output_pairs), &
-        n_bins => training_configuration%mini_batches(), &
-        adam => merge(.true., .false., training_configuration%optimizer_name() == "adam"), &
-        learning_rate => training_configuration%learning_rate() &
-      )
-        bins = [(bin_t(num_items=num_pairs, num_bins=n_bins, bin_number=b), b = 1, n_bins)]
+      print *,"Reading network outputs from " // network_output
 
-        print *,"Training network"
-        print *, "       Epoch   Cost (min)       Cost (max)       Cost (avg)"
+      associate(network_output_file => netCDF_file_t(network_output))
+        call network_output_file%input("potential_temperature", potential_temperature_out)
+        ! Skipping the following unnecessary outputs that are in the current file format as of 14 Aug 2023:
+        ! pressure, temperature, precipitation, snowfall
+        call network_output_file%input("qv", qv_out)
+        call network_output_file%input("qc", qc_out)
+        call network_output_file%input("qi", qi_out)
+        call network_output_file%input("qr", qr_out)
+        call network_output_file%input("qs", qs_out)
+        call network_output_file%input("time", time_out)
+        lbounds = [lbounds, lbound(qv_out), lbound(qc_out), lbound(qi_out), lbound(qr_out), lbound(qs_out)]
+        ubounds = [ubounds, ubounds_t(ubound(qv_out)), ubounds_t(ubound(qc_out)), ubounds_t(ubound(qi_out)), &
+          ubounds_t(ubound(qr_out)), ubounds_t(ubound(qs_out))]
+        call assert(all(lbounds == 1), "main: default input/output lower bounds", intrinsic_array_t(lbounds))
+        call assert(all(ubounds == ubounds(1)), "main: matching input/output upper bounds")
+        call assert(all(abs(time_in(2:t_end) - time_out(1:t_end-1))<tolerance), "main: matching time stamps")
+      end associate
 
-        do epoch = previous_epoch + 1, previous_epoch + num_epochs
+      print *,"Calculating time derivatives"
+  
+      allocate(dpt_dt, mold = potential_temperature_out)
+      allocate(dqv_dt, mold = qv_out)
+      allocate(dqc_dt, mold = qc_out)
+      allocate(dqi_dt, mold = qi_out)
+      allocate(dqr_dt, mold = qr_out)
+      allocate(dqs_dt, mold = qs_out)
 
-          call shuffle(input_output_pairs) ! set up for stochastic gradient descent
-          mini_batches = [(mini_batch_t(input_output_pairs(bins(b)%first():bins(b)%last())), b = 1, size(bins))]
-          call trainable_engine%train(mini_batches, cost, adam, learning_rate)
-          print *, epoch, minval(cost), maxval(cost), sum(cost)/size(cost)
-          write(plot_unit,*) epoch, minval(cost), maxval(cost), sum(cost)/size(cost)
-
-          open(newunit=network_unit, file=network_file, form='formatted', status='unknown', iostat=io_status, action='write')
-          associate(inference_engine => trainable_engine%to_inference_engine())
-            associate(json_file => inference_engine%to_json())
-              call json_file%write_lines(string_t(network_file))
-            end associate
-          end associate
-
-          close(network_unit)
-
-          inquire(file="stop-training", exist=stop_requested)
-
-          graceful_exit: &
-          if (stop_requested) then
-            print *,'Shutting down because a file named "stop-training" was found.'
-            return
-          end if graceful_exit
-
+      associate(dt => real(time_out - time_in, rkind))
+        do concurrent(t = 1:t_end)
+          dpt_dt(:,:,:,t) = (potential_temperature_out(:,:,:,t) - potential_temperature_in(:,:,:,t))/dt(t)
+          dqv_dt(:,:,:,t) = (qv_out(:,:,:,t)- qv_in(:,:,:,t))/dt(t)
+          dqc_dt(:,:,:,t) = (qc_out(:,:,:,t)- qc_in(:,:,:,t))/dt(t)
+          dqi_dt(:,:,:,t) = (qi_out(:,:,:,t)- qi_in(:,:,:,t))/dt(t)
+          dqr_dt(:,:,:,t) = (qr_out(:,:,:,t)- qr_in(:,:,:,t))/dt(t)
+          dqs_dt(:,:,:,t) = (qs_out(:,:,:,t)- qs_in(:,:,:,t))/dt(t)
         end do
       end associate
 
-    end block train_network
+      call assert(.not. any(ieee_is_nan(dpt_dt)), ".not. any(ieee_is_nan(dpt_dt)")
+      call assert(.not. any(ieee_is_nan(dqv_dt)), ".not. any(ieee_is_nan(dqv_dt)")
+      call assert(.not. any(ieee_is_nan(dqc_dt)), ".not. any(ieee_is_nan(dqc_dt)")
+      call assert(.not. any(ieee_is_nan(dqi_dt)), ".not. any(ieee_is_nan(dqi_dt)")
+      call assert(.not. any(ieee_is_nan(dqr_dt)), ".not. any(ieee_is_nan(dqr_dt)")
+      call assert(.not. any(ieee_is_nan(dqs_dt)), ".not. any(ieee_is_nan(dqs_dt)")
+
+      train_network: &
+      block
+        type(trainable_engine_t) trainable_engine
+        type(mini_batch_t), allocatable :: mini_batches(:)
+        type(bin_t), allocatable :: bins(:)
+        type(input_output_pair_t), allocatable :: input_output_pairs(:)
+        type(tensor_t), allocatable, dimension(:) :: inputs, outputs
+        real(rkind), parameter :: keep = 0.01
+        real(rkind), allocatable :: cost(:)
+        real(rkind), allocatable :: harvest(:)
+        integer i, batch, lon, lat, level, time, network_unit, io_status, final_step, epoch
+
+        open(newunit=network_unit, file=network_file, form='formatted', status='old', iostat=io_status, action='read')
+
+        if (io_status==0) then
+          print *,"Reading network from file " // network_file
+          trainable_engine = trainable_engine_t(inference_engine_t(file_t(string_t(network_file))))
+          close(network_unit)
+        else
+          close(network_unit)
+          print *,"Initializing a new network"
+          trainable_engine = new_engine(training_configuration, randomize=.true.)
+        end if
+
+        if (.not. allocated(end_step)) end_step = t_end
+        
+        print *,"Defining tensors from time step", start_step, "through", end_step, "with strides of", stride
+
+        ! The following temporary copies are required by gfortran bug 100650 and possibly 49324
+        ! See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100650 and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49324
+        inputs = [( [( [( [( &
+          tensor_t( &
+          [ pressure_in(lon,lat,level,time), potential_temperature_in(lon,lat,level,time), temperature_in(lon,lat,level,time),&
+            qv_in(lon,lat,level,time), qc_in(lon,lat,level,time), qi_in(lon,lat,level,time), qr_in(lon,lat,level,time), &
+            qs_in(lon,lat,level,time) &
+          ] &
+          ), lon = 1, size(qv_in,1))], lat = 1, size(qv_in,2))], level = 1, size(qv_in,3))], time = start_step, end_step, stride)]
+
+        outputs = [( [( [( [( &
+          tensor_t( &
+            [dpt_dt(lon,lat,level,time), dqv_dt(lon,lat,level,time), dqc_dt(lon,lat,level,time), &
+             dqi_dt(lon,lat,level,time), dqr_dt(lon,lat,level,time), dqs_dt(lon,lat,level,time) &
+            ] &
+          ), lon = 1, size(qv_in,1))], lat = 1, size(qv_in,2))], level = 1, size(qv_in,3))], time = start_step, end_step, stride)]
+        
+        print *, "Eliminating",int(100*(1.-keep)),"% of the grid points that have all-zero time derivatives"
+
+        associate(num_grid_pts => size(outputs))
+          if (allocated(harvest)) deallocate(harvest)
+          allocate(harvest(num_grid_pts))
+          call random_number(harvest)
+          associate(keepers => [(any(outputs(i)%values()/=0.) .or. harvest(i)<keep, i=1,num_grid_pts)])
+            input_output_pairs = input_output_pair_t(pack(inputs, keepers), pack(outputs, keepers))
+            print *, size(input_output_pairs), "points retained out of ", num_grid_pts, " points total"
+          end associate
+        end associate
+
+        associate( &
+          num_pairs => size(input_output_pairs), &
+          n_bins => training_configuration%mini_batches(), &
+          adam => merge(.true., .false., training_configuration%optimizer_name() == "adam"), &
+          learning_rate => training_configuration%learning_rate() &
+        )
+          bins = [(bin_t(num_items=num_pairs, num_bins=n_bins, bin_number=b), b = 1, n_bins)]
+
+          print *,"Training network"
+          print *, "       Epoch   Cost (min)       Cost (max)       Cost (avg)"
+
+          do epoch = previous_epoch + 1, previous_epoch + num_epochs
+
+            call shuffle(input_output_pairs) ! set up for stochastic gradient descent
+            mini_batches = [(mini_batch_t(input_output_pairs(bins(b)%first():bins(b)%last())), b = 1, size(bins))]
+            call trainable_engine%train(mini_batches, cost, adam, learning_rate)
+            print *, epoch, minval(cost), maxval(cost), sum(cost)/size(cost)
+            write(plot_unit,*) epoch, minval(cost), maxval(cost), sum(cost)/size(cost)
+
+            open(newunit=network_unit, file=network_file, form='formatted', status='unknown', iostat=io_status, action='write')
+            associate(inference_engine => trainable_engine%to_inference_engine())
+              associate(json_file => inference_engine%to_json())
+                call json_file%write_lines(string_t(network_file))
+              end associate
+            end associate
+
+            close(network_unit)
+
+            inquire(file="stop-training", exist=stop_requested)
+
+            graceful_exit: &
+            if (stop_requested) then
+              print *,'Shutting down because a file named "stop-training" was found.'
+              return
+            end if graceful_exit
+
+          end do
+        end associate
+
+      end block train_network
 
     end associate
 
