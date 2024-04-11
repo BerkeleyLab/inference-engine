876ae89f3123e49fc9da113f02cb3d832ecfaaed
Katherine Rasmussen <krasmussen@lbl.gov>
2024-04-11 16:45:41 -0700

    Update more macros in `training_configuration_s.F90` to include cray fortran compiler in the workaround for associate-stmt.

--- src/inference_engine/training_configuration_s.F90
+++ src/inference_engine/training_configuration_s.F90
@@ -79,12 +79,10 @@ contains
   end procedure
 
   module procedure differentiable_activation_strategy
-#ifdef __INTEL_COMPILER
+#if defined __INTEL_COMPILER || _CRAYFTN
     type(string_t) :: activation_name
     activation_name = self%network_configuration_%activation_name()
-#endif
-
-#ifndef __INTEL_COMPILER
+#else
     associate(activation_name => self%network_configuration_%activation_name())
 #endif
       select case(activation_name%string())
@@ -97,7 +95,8 @@ contains
         case default
           error stop 'activation_strategy_factory_s(factory): unrecognized activation name "' // activation_name%string() // '"' 
       end select
-#ifndef __INTEL_COMPILER
+#if defined __INTEL_COMPILER || _CRAYFTN
+#else
     end associate
 #endif
   end procedure
