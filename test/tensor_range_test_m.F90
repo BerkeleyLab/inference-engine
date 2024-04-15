515688add118a908051157185ecb9073ade019dd
Katherine Rasmussen <krasmussen@lbl.gov>
2024-04-15 14:21:40 -0700

    Add more workarounds to cray bug with associate-stmt

--- test/tensor_range_test_m.F90
+++ test/tensor_range_test_m.F90
@@ -72,7 +72,14 @@ contains
   function map_to_from_training_range() result(test_passes)
     logical test_passes
     real, parameter :: tolerance = 1.E-07
-
+#ifdef _CRAYFTN
+    type(tensor_range_t) :: tensor_range
+    type(tensor_t) :: tensor, round_trip
+    tensor_range = tensor_range_t(layer="output", minima=[-4., 0., 1., -1.], maxima=[0., 2., 5., 1.])
+    tensor = tensor_t([-2., 0., 5., 0.])
+    round_trip = tensor_range%map_from_training_range(tensor_range%map_to_training_range(tensor))
+    test_passes = all(abs(tensor%values() - round_trip%values()) < tolerance)
+#else
     associate(tensor_range => tensor_range_t(layer="output", minima=[-4., 0., 1., -1.], maxima=[0., 2., 5., 1.]))
       associate(tensor => tensor_t([-2., 0., 5., 0.]))
         associate(round_trip => tensor_range%map_from_training_range(tensor_range%map_to_training_range(tensor)))
@@ -80,6 +87,7 @@ contains
         end associate
       end associate
     end associate
+#endif
   end function
 
 end module tensor_range_test_m
