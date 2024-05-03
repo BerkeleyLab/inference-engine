207a075bcc069a99ad5f96123930b9e626b34f80
Damian Rouson <damian@rouson.net>
2024-05-03 11:47:24 -0700

    repro: rm tensor_m type-bound procs & tensor_s

--- src/tensor_m.f90
+++ src/tensor_m.f90
@@ -3,28 +3,7 @@
 module tensor_m
   use kind_parameters_m, only : rkind
   implicit none
-
   type tensor_t
     real(rkind), allocatable :: values_(:)
-  contains
-    procedure values
-    procedure num_components
   end type
-
-  interface
-
-    pure module function values(self) result(tensor_values)
-      implicit none
-      class(tensor_t), intent(in) :: self
-      real(rkind), allocatable :: tensor_values(:)
-    end function
-
-    pure module function num_components(self) result(n)
-      implicit none
-      class(tensor_t), intent(in) :: self
-      integer n
-    end function
-
-  end interface
-
 end module tensor_m
