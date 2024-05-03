ca63607841ae7d1c796e24d3736c050c049a43d6
Damian Rouson <damian@rouson.net>
2024-05-03 12:48:49 -0700

    repro: rm all string_t type-bound procs but 1

--- src/sourcery_string_m.f90
+++ src/sourcery_string_m.f90
@@ -4,47 +4,11 @@ module sourcery_string_m
   type string_t
     character(len=:), allocatable :: string_
   contains
-    generic :: get_json_value =>     get_json_integer_array, get_json_logical, get_json_integer, get_json_string, get_json_real, &
-                                     get_json_real_array
-    procedure, private            :: get_json_integer_array, get_json_logical, get_json_integer, get_json_string, get_json_real, &
-                                     get_json_real_array
+    generic :: get_json_value => get_json_real_array
+    procedure get_json_real_array 
   end type
 
   interface
-    pure module function get_json_real(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      real, intent(in) :: mold
-      real value_
-    end function
-
-    elemental module function get_json_string(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key, mold
-      type(string_t) :: value_
-    end function
-
-    pure module function get_json_integer(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      integer, intent(in) ::  mold
-      integer value_
-    end function
-
-    elemental module function get_json_logical(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      logical, intent(in) :: mold
-      logical value_
-    end function
-
-    pure module function get_json_integer_array(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      integer, intent(in) :: mold(:)
-      integer, allocatable :: value_(:)
-    end function
-
     pure module function get_json_real_array(self, key, mold) result(value_)
       implicit none
       class(string_t), intent(in) :: self, key
@@ -52,4 +16,5 @@ module sourcery_string_m
       real, allocatable :: value_(:)
     end function
   end interface
+
 end module sourcery_string_m
--- src/sourcery_string_s.f90
+++ src/sourcery_string_s.f90
@@ -1,85 +1,10 @@
 submodule(sourcery_string_m) sourcery_string_s
   implicit none
 contains
-  module procedure get_json_real
-    character(len=:), allocatable :: raw_line, string_value
-
-    raw_line = self%string_
-    associate(text_after_colon => raw_line(index(raw_line, ':')+1:))
-      associate(trailing_comma => index(text_after_colon, ','))
-        if (trailing_comma == 0) then
-          string_value = trim(adjustl((text_after_colon)))
-        else 
-          string_value = trim(adjustl((text_after_colon(:trailing_comma-1))))
-        end if
-        read(string_value, fmt=*) value_
-      end associate
-    end associate
-
-  end procedure
-
-  module procedure get_json_string
-
-    character(len=:), allocatable :: raw_line
-
-    raw_line = self%string_
-    associate(text_after_colon => raw_line(index(raw_line, ':')+1:))
-      associate(opening_value_quotes => index(text_after_colon, '"'))
-        associate(closing_value_quotes => opening_value_quotes + index(text_after_colon(opening_value_quotes+1:), '"'))
-          if (any([opening_value_quotes, closing_value_quotes] == 0)) then
-            value_ = string_t(trim(adjustl((text_after_colon))))
-          else
-            value_ = string_t(text_after_colon(opening_value_quotes+1:closing_value_quotes-1))
-          end if
-        end associate
-      end associate
-    end associate
-
-  end procedure
-
-  module procedure get_json_logical
-    character(len=:), allocatable :: raw_line, string_value
-
-    raw_line = self%string_
-    associate(text_after_colon => raw_line(index(raw_line, ':')+1:))
-      associate(trailing_comma => index(text_after_colon, ','))
-        if (trailing_comma == 0) then
-          string_value = trim(adjustl((text_after_colon)))
-        else 
-          string_value = trim(adjustl((text_after_colon(:trailing_comma-1))))
-        end if
-        value_ = string_value == "true"
-      end associate
-    end associate
-
-  end procedure
-
-  module procedure get_json_integer
-    character(len=:), allocatable :: raw_line, string_value
-
-    raw_line = self%string_
-    associate(text_after_colon => raw_line(index(raw_line, ':')+1:))
-      associate(trailing_comma => index(text_after_colon, ','))
-        if (trailing_comma == 0) then
-          string_value = trim(adjustl((text_after_colon)))
-        else 
-          string_value = trim(adjustl((text_after_colon(:trailing_comma-1))))
-        end if
-        read(string_value, fmt=*) value_
-      end associate
-    end associate
-
-  end procedure
-
-  module procedure get_json_integer_array
-    value_ = int(self%get_json_real_array(key,mold=[0.]))
-  end procedure
-
   module procedure get_json_real_array
     character(len=:), allocatable :: raw_line
     real, allocatable :: real_array(:)
     integer i
-
     raw_line = self%string_
     associate(colon => index(raw_line, ":"))
       associate(opening_bracket => colon + index(raw_line(colon+1:), "["))
@@ -94,6 +19,5 @@ contains
         end associate
       end associate
     end associate
-
   end procedure
 end submodule
