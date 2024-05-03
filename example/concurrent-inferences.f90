5aabeeac959f373aeeb2fb40fa4f89bad143cd4c
Damian Rouson <damian@rouson.net>
2024-05-02 23:10:25 -0700

    ICE on do concurrent with auto-offload

--- example/concurrent-inferences.f90
+++ example/concurrent-inferences.f90
@@ -12,8 +12,15 @@ program concurrent_inferences
   real, allocatable :: input_components(:,:,:,:)
   integer, parameter :: lat=263, lon=317, lev=15 ! latitudes, longitudes, levels (elevations)
   integer i, j, k
+  integer, parameter :: nodes_per_layer(*) = [2, 3, 1]
+ integer, parameter :: max_n = maxval(nodes_per_layer), layers = size(nodes_per_layer)
 
-  !inference_engine = inference_engine_t(file_t(network_file_name))
+  inference_engine = inference_engine_t( &
+    metadata = [string_t("XOR"), string_t("Damian Rouson"), string_t("2023-07-02"), string_t("step"), string_t("false")], &
+    weights = reshape([real:: [1,1,0, 0,1,1, 0,0,0], [1,0,0, -2,0,0, 1,0,0]], [max_n, max_n, layers-1]), &
+    biases = reshape([[0.,-1.99,0.], [0., 0., 0.]], [max_n, layers-1]), &
+    nodes = nodes_per_layer &
+  )
 
   allocate(inputs(lat,lon,lev))
   allocate(outputs(lat,lon,lev))
