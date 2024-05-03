7ff9a0bd51dcc39b95d5132a2016cec14f0b9217
Damian Rouson <damian@rouson.net>
2024-05-03 15:21:13 -0700

    repro: rm tensor_t

--- example/concurrent-inferences.f90
+++ example/concurrent-inferences.f90
@@ -2,22 +2,18 @@ program concurrent_inferences
   use string_m
   implicit none
 
-  type tensor_t
-    real t
-  end type
-
   type inference_engine_t
     type(string_t), allocatable :: metadata(:)
   end type
 
-  type(tensor_t) outputs(1)
+  real inferences(1)
   type(inference_engine_t) inference_engine
   integer i
 
   inference_engine%metadata = [string_t("")]
 
-  do concurrent(i=1:size(outputs))
-    outputs(i)%t = infer(inference_engine)           
+  do concurrent(i=1:size(inferences))
+    inferences(i) = infer(inference_engine)           
   end do
 
 contains
