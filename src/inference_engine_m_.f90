2fae3dc80c45cb0d19aefdece651456b5cd6c072
Damian Rouson <damian@rouson.net>
2024-05-03 10:50:26 -0700

    reproducer: rm tensor_range_{m,s}

--- src/inference_engine_m_.f90
+++ src/inference_engine_m_.f90
@@ -6,7 +6,6 @@ module inference_engine_m_
   use sourcery_string_m, only : string_t
   use kind_parameters_m, only : rkind
   use tensor_m, only : tensor_t
-  use tensor_range_m, only : tensor_range_t
   implicit none
 
   private
@@ -19,7 +18,6 @@ module inference_engine_m_
   type inference_engine_t
     !! Encapsulate the minimal information needed to perform inference
     private
-    type(tensor_range_t) input_range_, output_range_
     type(string_t) metadata_(size(key))
     real(rkind), allocatable :: weights_(:,:,:), biases_(:,:)
     integer, allocatable :: nodes_(:)
@@ -31,35 +29,18 @@ module inference_engine_m_
 
   interface inference_engine_t
 
-    pure module function construct_from_padded_arrays(metadata, weights, biases, nodes, input_range, output_range) &
+    pure module function construct_from_padded_arrays(metadata, weights, biases, nodes) &
       result(inference_engine)
       implicit none
       type(string_t), intent(in) :: metadata(:)
       real(rkind), intent(in) :: weights(:,:,:), biases(:,:)
       integer, intent(in) :: nodes(0:)
-      type(tensor_range_t), intent(in), optional :: input_range, output_range
       type(inference_engine_t) inference_engine
     end function
 
   end interface
 
   interface
-
-    elemental module function map_to_input_range(self, tensor) result(normalized_tensor)
-      !! The result contains the input tensor values normalized to fall on the range used during training
-      implicit none
-      class(inference_engine_t), intent(in) :: self
-      type(tensor_t), intent(in) :: tensor
-      type(tensor_t) normalized_tensor
-    end function
-
-    elemental module function map_from_output_range(self, normalized_tensor) result(tensor)
-      !! The result contains the output tensor values unnormalized via the inverse of the mapping used in training
-      implicit none
-      class(inference_engine_t), intent(in) :: self
-      type(tensor_t), intent(in) :: normalized_tensor
-      type(tensor_t) tensor
-    end function
 
     elemental module function infer(self, inputs) result(outputs)
       implicit none
--- src/inference_engine_s.f90
+++ src/inference_engine_s.f90
@@ -4,83 +4,33 @@ submodule(inference_engine_m_) inference_engine_s
   use step_m, only : step_t
   use sourcery_formats_m, only : separated_values
   implicit none
-
 contains
-
-  module procedure map_to_input_range
-    normalized_tensor = self%input_range_%map_to_training_range(tensor)
-  end procedure
-
-  module procedure map_from_output_range
-    tensor = self%output_range_%map_from_training_range(normalized_tensor)
-  end procedure
-
   module procedure infer
-
     real(rkind), allocatable :: a(:,:)
     integer, parameter :: input_layer = 0
     integer k, l
-
     associate(w => self%weights_, b => self%biases_, n => self%nodes_, output_layer => ubound(self%nodes_,1))
-
       allocate(a(maxval(n), input_layer:output_layer))
-
-      associate(normalized_inputs => self%input_range_%map_to_training_range(inputs))
-        a(1:n(input_layer),input_layer) = normalized_inputs%values()
-      end associate
-
+      a(1:n(input_layer),input_layer) = inputs%values()
       feed_forward: &
       do l = input_layer+1, output_layer
         associate(z => matmul(w(1:n(l),1:n(l-1),l), a(1:n(l-1),l-1)) + b(1:n(l),l))
           a(1:n(l),l) = self%activation_strategy_%activation(z)
         end associate
       end do feed_forward
-
-      associate(normalized_outputs => tensor_t(a(1:n(output_layer), output_layer)))
-        outputs = self%output_range_%map_from_training_range(normalized_outputs)
-      end associate
-
+      outputs = tensor_t(a(1:n(output_layer), output_layer))
     end associate
-
   end procedure
 
   module procedure construct_from_padded_arrays
-
     inference_engine%metadata_ = metadata
     inference_engine%weights_ = weights
     inference_engine%biases_ = biases
     inference_engine%nodes_ = nodes
-
-    block
-      integer i
-
-      if (present(input_range)) then
-        inference_engine%input_range_ = input_range
-      else
-        associate(num_inputs => nodes(lbound(nodes,1)))
-          associate(default_minima => [(0., i=1,num_inputs)], default_maxima => [(1., i=1,num_inputs)])
-            inference_engine%input_range_ = tensor_range_t("inputs", default_minima, default_maxima)
-          end associate
-        end associate
-      end if
-
-      if (present(output_range)) then
-        inference_engine%output_range_ = output_range
-      else
-        associate(num_outputs => nodes(ubound(nodes,1)))
-          associate(default_minima => [(0., i=1,num_outputs)], default_maxima => [(1., i=1,num_outputs)])
-            inference_engine%output_range_ = tensor_range_t("outputs", default_minima, default_maxima)
-          end associate
-        end associate
-      end if
-    end block
-
     inference_engine%activation_strategy_ = step_t()
-
   end procedure construct_from_padded_arrays
 
   module procedure num_inputs
     input_count = self%nodes_(lbound(self%nodes_,1))
   end procedure
-
 end submodule inference_engine_s
