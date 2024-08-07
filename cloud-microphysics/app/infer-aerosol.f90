764ab165e78bd4b0394fe6b55e20f27c3fbb01b6
Carolyn Torres <carolyntorres@d0-88-c-82-d3-e3.dhcp.lbnl.us>
2024-08-07 16:04:28 -0700

    Updated branch to allow Openmp parallelism

--- cloud-microphysics/app/infer-aerosol.f90
+++ cloud-microphysics/app/infer-aerosol.f90
@@ -7,18 +7,20 @@ program infer_aerosol
   use julienne_m, only : string_t, file_t, command_line_t
   use assert_m, only : assert
   use iso_fortran_env, only : int64, real64
+  use OMP_LIB
 
   !! Internal dependencies;
   use inference_engine_m, only : inference_engine_t, tensor_t, infer
   use NetCDF_file_m, only: NetCDF_file_t
   implicit none
 
-  integer i,j,num_inputs,num_outputs
+  integer icc,i,j,num_inputs,num_outputs,thread_id
   type(string_t) network_file_name
   type(command_line_t) command_line
   type(inference_engine_t) network,inference_engine
   real, allocatable, dimension(:,:) :: aerosol_data
   real, allocatable, dimension(:) :: meanx, meany, stdx, stdy
+  double precision start_time,end_time
   real cube_root,signa
 
   type(tensor_t), allocatable :: inputs(:), outputs(:)
@@ -64,6 +66,17 @@ program infer_aerosol
   end do
 
   call get_command_line_arguments(base_name) 
+
+!  !$OMP PARALLEL PRIVATE(thread_id)                                                                 
+!  thread_id = OMP_GET_THREAD_NUM()
+
+!  do i=0, OMP_GET_MAX_THREADS()
+!     if (i == thread_id) then
+!        print *, "Hello OpenMP from thread: ", thread_id
+!     end if
+!     !$OMP BARRIER                                                                                  
+!  end do
+!  !$OMP END PARALLEL    
   
   associate( &
       network_input => base_name // "_input.nc", &
@@ -80,49 +93,65 @@ program infer_aerosol
       allocate(output_components(size(aerosol_data,2),31))
 
       ! Pre-processing
-      do i = 1,size(aerosol_data,1)
-         do j = 1,size(aerosol_data,2)
+!$omp parallel do shared(aerosol_data,input_components,meanx,stdx) private(i,j,signa,cube_root)
+      do j = 1,size(aerosol_data,2)
+         do i = 1,size(aerosol_data,1)
             signa = sign(1.0,aerosol_data(i,j))
             cube_root = signa*(abs(aerosol_data(i,j))**(1.d0/3.d0))
             input_components(j,i) = (cube_root - meanx(i))/stdx(i)
          end do
       end do
+!$omp end parallel do   
       
       network_file_name = string_t(command_line%flag_value("--network"))
  
       print *, "Constructing a new inference_engine_t object from the file " // network_file_name%string()
       inference_engine = inference_engine_t(file_t(network_file_name))
 
-      do concurrent(i=1:size(input_components,1))
+!$omp parallel do shared(inputs,input_components) 
+      !      do concurrent(i=1:size(input_components,1))
+      do i = 1,size(input_components,1)
          inputs(i) = tensor_t(input_components(i,:))
       end do
-      
-      call system_clock(t_start, clock_rate)      
-      !      outputs = inference_engine%infer(inputs)  ! implicit allocation of outputs
+!$omp end parallel do         
+
       print*,'Inference'
+      call system_clock(t_start, clock_rate)      
+
       ! Inference
-      do i = 1,size(input_components,1)
+
+      icc = size(input_components,1)
+!$    start_time = omp_get_wtime()
+      !$omp parallel do shared(inputs,outputs,icc)
+!      outputs = inference_engine%infer(inputs)  ! implicit allocation of outputs
+      do i = 1,icc
          outputs(i) = inference_engine%infer_na(inputs(i))
       end do
-      print*,'End inference'
+!$omp end parallel do   
+!$    end_time = omp_get_wtime()       
       call system_clock(t_finish)
+      print*,'End inference'
+      
       
       allocate(output_slice(num_outputs))
 
       ! Post-processing
-      do i = 1,size(aerosol_data,2)
+!$omp parallel do shared(outputs,output_components,icc,meany,stdy) private(output_slice,i,j)      
+      do i = 1,icc
          output_slice = outputs(i)%values()
          do j = 1,num_outputs
             output_components(i,j) = (stdy(j)*output_slice(j)+meany(j))**3
-            if (i .eq. 1) print*,'output slice = ',output_components(i,j)
+!            if (i .eq. 1) print*,'output slice = ',output_components(i,j)
          end do
       end do
+!$omp end parallel do         
    
     end associate
   end associate    
   
   
   print *,"System clock time: ", real(t_finish - t_start, real64)/real(clock_rate, real64)
+!$  print*,'OMP Total time = ',end_time - start_time
   print *,new_line('a') // "______inference_aerosol done _______"
 
 contains
