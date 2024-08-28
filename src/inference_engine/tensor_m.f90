c63d9bbba880b276df69152d41f55f46672df9b7
Damian Rouson <rouson@lbl.gov>
2024-08-27 19:51:06 -0700

    refac(tensor_m): add kind parameter

--- src/inference_engine/tensor_m.f90
+++ src/inference_engine/tensor_m.f90
@@ -1,15 +1,15 @@
 ! Copyright (c), The Regents of the University of California
 ! Terms of use are as specified in LICENSE.txt
 module tensor_m
-  use kind_parameters_m, only : rkind
+  use kind_parameters_m, only : default_real
   implicit none
 
   private
   public :: tensor_t
 
-  type tensor_t
-    private
-    real(rkind), allocatable :: values_(:)
+  type tensor_t(k)
+    integer, kind :: k = default_real 
+    real, allocatable, private :: values_(:)
   contains
     procedure values
     procedure num_components
@@ -19,7 +19,7 @@ module tensor_m
 
     pure module function construct_from_components(values) result(tensor)
       implicit none
-      real(rkind), intent(in) :: values(:)
+      real, intent(in) :: values(:)
       type(tensor_t) tensor
     end function
 
@@ -30,7 +30,7 @@ module tensor_m
     pure module function values(self) result(tensor_values)
       implicit none
       class(tensor_t), intent(in) :: self
-      real(rkind), allocatable :: tensor_values(:)
+      real, allocatable :: tensor_values(:)
     end function
 
     pure module function num_components(self) result(n)
