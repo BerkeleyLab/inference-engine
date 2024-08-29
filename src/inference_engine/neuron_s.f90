85fba964ededb281f3c81776e8b6193e7ca54569
Damian Rouson <rouson@lbl.gov>
2024-08-29 00:58:17 -0700

    feat(neuron_t): add double-precision procedures

--- src/inference_engine/neuron_s.f90
+++ src/inference_engine/neuron_s.f90
@@ -27,6 +27,26 @@ contains
     ]
   end procedure
 
+  module procedure double_precision_to_json
+    integer, parameter :: characters_per_value=34
+    character(len=*), parameter :: indent = repeat(" ",ncopies=12)
+    character(len=:), allocatable :: csv_format, weights_string, bias_string
+
+    call assert(allocated(self%weights_), "neuron_s(to_json): allocated weights_")
+
+    csv_format = separated_values(separator=",", mold=[double precision::])
+    allocate(character(len=size(self%weights_)*(characters_per_value+1)-1)::weights_string)
+    allocate(character(len=characters_per_value)::bias_string)
+    write(weights_string, fmt = csv_format) self%weights_
+    write(bias_string,*) self%bias_
+    lines = [ &
+      string_t(indent // '{'), &
+      string_t(indent // '  "weights": [' // trim(adjustl(weights_string)) // '],'), &
+      string_t(indent // '  "bias": ' // trim(adjustl(bias_string))), &
+      string_t(indent // '}') &
+    ]
+  end procedure
+
   module procedure from_json
 
     character(len=:), allocatable :: line
@@ -62,7 +82,12 @@ contains
 
   end procedure
 
-  module procedure from_components
+  module procedure default_real_from_components
+    neuron%weights_ = weights
+    neuron%bias_ = bias
+  end procedure
+
+  module procedure double_precision_from_components
     neuron%weights_ = weights
     neuron%bias_ = bias
   end procedure
@@ -71,11 +96,23 @@ contains
     my_weights = self%weights_
   end procedure
 
+  module procedure double_precision_weights
+    my_weights = self%weights_
+  end procedure
+
   module procedure default_real_bias
     my_bias = self%bias_
   end procedure
 
+  module procedure double_precision_bias
+    my_bias = self%bias_
+  end procedure
+
   module procedure default_real_next_allocated
+    next_is_allocated = allocated(self%next)
+  end procedure
+
+  module procedure double_precision_next_allocated
     next_is_allocated = allocated(self%next)
   end procedure
 
@@ -87,7 +124,19 @@ contains
     end if
   end procedure
 
+  module procedure double_precision_next_pointer
+    if (allocated(self%next)) then
+      next_ptr => self%next
+    else
+      next_ptr => null()
+    end if
+  end procedure
+
   module procedure default_real_num_inputs
+    size_weights = size(self%weights_)
+  end procedure
+
+  module procedure double_precision_num_inputs
     size_weights = size(self%weights_)
   end procedure
 
