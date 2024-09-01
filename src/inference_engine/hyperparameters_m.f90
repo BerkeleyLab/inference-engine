7ea4e3bbf3cc2c2687a4a7ecd04fbe2e1a260054
Damian Rouson <rouson@lbl.gov>
2024-08-31 23:53:30 -0700

    feat(hyperparameters): support double precision
    also update julienne dependency to 1.2.0 for double-precsion value
    extraction from JSON files.

--- src/inference_engine/hyperparameters_m.f90
+++ src/inference_engine/hyperparameters_m.f90
@@ -1,34 +1,44 @@
 module hyperparameters_m
   use julienne_string_m, only : string_t
-  use kind_parameters_m, only : rkind
+  use kind_parameters_m, only : default_real, double_precision
+  use double_precision_string_m, only : double_precision_string_t
   implicit none
-
   private
   public :: hyperparameters_t
 
-  type hyperparameters_t
-    private
-    integer :: mini_batches_ = 10
-    real :: learning_rate_ = 1.5
+  type hyperparameters_t(k)
+    integer, kind :: k = default_real
+    integer, private:: mini_batches_ = 10
+    real(k), private :: learning_rate_ = 1.5_k
     character(len=:), allocatable :: optimizer_
   contains
-    procedure :: to_json
-    procedure :: equals
-    generic :: operator(==) => equals
-    procedure :: mini_batches
-    procedure :: optimizer_name
-    procedure :: learning_rate
-    end type
+    generic :: to_json => default_real_to_json, double_precision_to_json
+    procedure, private :: default_real_to_json, double_precision_to_json
+    generic :: operator(==) => default_real_equals, double_precision_equals
+    procedure, private ::      default_real_equals, double_precision_equals
+    generic :: mini_batches => default_real_mini_batches, double_precision_mini_batches
+    procedure, private ::      default_real_mini_batches, double_precision_mini_batches
+    generic :: optimizer_name => default_real_optimizer_name, double_precision_optimizer_name
+    procedure, private ::        default_real_optimizer_name, double_precision_optimizer_name
+    generic :: learning_rate => default_real_learning_rate, double_precision_learning_rate
+    procedure, private ::       default_real_learning_rate, double_precision_learning_rate
+  end type
 
   interface hyperparameters_t
 
-    pure module function from_json(lines) result(hyperparameters)
+    pure module function default_real_from_json(lines) result(hyperparameters)
       implicit none
       type(string_t), intent(in) :: lines(:)
       type(hyperparameters_t) hyperparameters
     end function
 
-    pure module function from_components(mini_batches, learning_rate, optimizer) result(hyperparameters)
+    pure module function double_precision_from_json(lines) result(hyperparameters)
+      implicit none
+      type(double_precision_string_t), intent(in) :: lines(:)
+      type(hyperparameters_t(double_precision)) hyperparameters
+    end function
+
+    pure module function default_real_from_components(mini_batches, learning_rate, optimizer) result(hyperparameters)
       implicit none
       integer, intent(in) :: mini_batches
       real, intent(in) :: learning_rate
@@ -36,40 +46,78 @@ module hyperparameters_m
       type(hyperparameters_t) hyperparameters
     end function
 
+    pure module function double_precision_from_components(mini_batches, learning_rate, optimizer) result(hyperparameters)
+      implicit none
+      integer, intent(in) :: mini_batches
+      double precision, intent(in) :: learning_rate
+      character(len=*), intent(in) :: optimizer
+      type(hyperparameters_t(double_precision)) hyperparameters
+    end function
+
   end interface
 
   interface
 
-    pure module function to_json(self) result(lines)
+    pure module function default_real_to_json(self) result(lines)
       implicit none
       class(hyperparameters_t), intent(in) :: self
       type(string_t), allocatable :: lines(:)
     end function
 
-    elemental module function equals(lhs, rhs) result(lhs_equals_rhs)
+    pure module function double_precision_to_json(self) result(lines)
+      implicit none
+      class(hyperparameters_t(double_precision)), intent(in) :: self
+      type(string_t), allocatable :: lines(:)
+    end function
+
+    elemental module function default_real_equals(lhs, rhs) result(lhs_equals_rhs)
       implicit none
       class(hyperparameters_t), intent(in) :: lhs, rhs
       logical lhs_equals_rhs
     end function
 
-    elemental module function mini_batches(self) result(num_mini_batches)
+    elemental module function double_precision_equals(lhs, rhs) result(lhs_equals_rhs)
+      implicit none
+      class(hyperparameters_t(double_precision)), intent(in) :: lhs, rhs
+      logical lhs_equals_rhs
+    end function
+
+    elemental module function default_real_mini_batches(self) result(num_mini_batches)
       implicit none
       class(hyperparameters_t), intent(in) :: self
       integer num_mini_batches
     end function
 
-    elemental module function optimizer_name(self) result(identifier)
+    elemental module function double_precision_mini_batches(self) result(num_mini_batches)
+      implicit none
+      class(hyperparameters_t(double_precision)), intent(in) :: self
+      integer num_mini_batches
+    end function
+
+     elemental module function default_real_learning_rate(self) result(rate)
+       implicit none
+       class(hyperparameters_t), intent(in) :: self
+       real rate
+     end function
+
+     elemental module function double_precision_learning_rate(self) result(rate)
+       implicit none
+       class(hyperparameters_t(double_precision)), intent(in) :: self
+       double precision rate
+     end function
+
+    elemental module function default_real_optimizer_name(self) result(identifier)
       implicit none
       class(hyperparameters_t), intent(in) :: self
       type(string_t) identifier
      end function
 
-
-     elemental module function learning_rate(self) result(rate)
-       implicit none
-       class(hyperparameters_t), intent(in) :: self
-       real(rkind) rate
+    elemental module function double_precision_optimizer_name(self) result(identifier)
+      implicit none
+      class(hyperparameters_t(double_precision)), intent(in) :: self
+      type(string_t) identifier
      end function
+
   end interface
 
 end module
--- src/inference_engine/hyperparameters_s.f90
+++ src/inference_engine/hyperparameters_s.f90
@@ -8,17 +8,38 @@ submodule(hyperparameters_m) hyperparameters_s
 
 contains
 
-  module procedure from_components
+  module procedure default_real_from_components
     hyperparameters%mini_batches_ = mini_batches
     hyperparameters%learning_rate_ = learning_rate
     hyperparameters%optimizer_ = optimizer
   end procedure 
 
-  module procedure equals
+  module procedure double_precision_from_components
+    hyperparameters%mini_batches_ = mini_batches
+    hyperparameters%learning_rate_ = learning_rate
+    hyperparameters%optimizer_ = optimizer
+  end procedure
+
+  module procedure default_real_equals
 
     real, parameter :: tolerance = 1.E-08
 
-    call assert(allocated(lhs%optimizer_) .and. allocated(rhs%optimizer_), "hyperparameters_s(equals): allocated optimizers")
+    call assert(allocated(lhs%optimizer_) .and. allocated(rhs%optimizer_), &
+      "hyperparameters_s(default_real_equals): allocated optimizers")
+
+    lhs_equals_rhs = &
+      lhs%mini_batches_ == rhs%mini_batches_ .and. &
+      lhs%optimizer_ == rhs%optimizer_ .and. &
+      abs(lhs%learning_rate_ - rhs%learning_rate_) <= tolerance
+
+  end procedure
+
+  module procedure double_precision_equals
+
+    double precision, parameter :: tolerance = 1.D-15
+
+    call assert(allocated(lhs%optimizer_) .and. allocated(rhs%optimizer_), &
+      "hyperparameters_s(double_precisionequals): allocated optimizers")
 
     lhs_equals_rhs = &
       lhs%mini_batches_ == rhs%mini_batches_ .and. &
@@ -27,7 +48,7 @@ contains
      
   end procedure 
 
-  module procedure from_json
+  module procedure default_real_from_json
     integer l
     logical hyperparameters_key_found 
 
@@ -43,10 +64,29 @@ contains
       end if
     end do
 
-    call assert(hyperparameters_key_found, "hyperparameters_s(from_json): hyperparameters_found")
+    call assert(hyperparameters_key_found, "hyperparameters_s(default_real_from_json): hyperparameters_found")
   end procedure
 
-  module procedure to_json
+  module procedure double_precision_from_json
+    integer l
+    logical hyperparameters_key_found 
+
+    hyperparameters_key_found = .false.
+
+    do l=1,size(lines)
+      if (lines(l)%get_json_key() == "hyperparameters") then
+        hyperparameters_key_found = .true.
+        hyperparameters%mini_batches_  = lines(l+1)%get_json_value(string_t(mini_batches_key), mold=0)
+        hyperparameters%learning_rate_ = lines(l+2)%get_json_value(string_t(learning_rate_key), mold=0.D0)
+        hyperparameters%optimizer_ = lines(l+3)%get_json_value(string_t(optimizer_key), mold=string_t(""))
+        return
+      end if
+    end do
+
+    call assert(hyperparameters_key_found, "hyperparameters_s(double_precision_from_json): hyperparameters_found")
+  end procedure
+
+  module procedure default_real_to_json
     character(len=*), parameter :: indent = repeat(" ",ncopies=4)
     integer, parameter :: max_width= 18
     character(len=max_width) mini_batches_string, learning_rate_string
@@ -63,15 +103,44 @@ contains
     ]
   end procedure
 
-  module procedure mini_batches
+  module procedure double_precision_to_json
+    character(len=*), parameter :: indent = repeat(" ",ncopies=4)
+    integer, parameter :: max_width= 36
+    character(len=max_width) mini_batches_string, learning_rate_string
+
+    write(mini_batches_string,*) self%mini_batches_
+    write(learning_rate_string,*) self%learning_rate_
+
+    lines = [ &
+      string_t(indent // '"hyperparameters": {'), &
+      string_t(indent // indent // '"' // mini_batches_key  // '" : '  // trim(adjustl(mini_batches_string))  // "," ), &
+      string_t(indent // indent // '"' // learning_rate_key // '" : '  // trim(adjustl(learning_rate_string)) // "," ), &
+      string_t(indent // indent // '"' // optimizer_key     // '" : "' // trim(adjustl(self%optimizer_     )) // '"'), &
+      string_t(indent // '}') &
+    ]
+  end procedure
+
+  module procedure default_real_mini_batches
     num_mini_batches = self%mini_batches_
   end procedure
 
-  module procedure optimizer_name
+  module procedure double_precision_mini_batches
+    num_mini_batches = self%mini_batches_
+  end procedure
+
+  module procedure default_real_optimizer_name
     identifier = string_t(self%optimizer_)
   end procedure
 
-  module procedure learning_rate
+  module procedure double_precision_optimizer_name
+    identifier = string_t(self%optimizer_)
+  end procedure
+
+  module procedure default_real_learning_rate
+    rate = self%learning_rate_
+  end procedure
+
+  module procedure double_precision_learning_rate
     rate = self%learning_rate_
   end procedure
 
