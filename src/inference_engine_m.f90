3abb069a58501a0a72bd6e89bcff9f8c04cac557
Damian Rouson <rouson@lbl.gov>
2024-09-02 17:31:42 -0700

    fix(inference_engine_m): make entities public
    This commit exposes the double_precision kind parameter and the
    double_precision_string_t in the inference_engine_m module.

--- src/inference_engine_m.f90
+++ src/inference_engine_m.f90
@@ -4,13 +4,15 @@ module inference_engine_m
  !! Specify the user-facing modules, derived types, and type parameters
  use activation_strategy_m, only : activation_strategy_t
  use differentiable_activation_strategy_m, only : differentiable_activation_strategy_t
+ use double_precision_string_m, only : double_precision_string_t
+ use gelu_m, only : gelu_t
  use hyperparameters_m, only : hyperparameters_t
  use input_output_pair_m, only : input_output_pair_t, shuffle, write_to_stdout
  use inference_engine_m_, only : inference_engine_t
+ use kind_parameters_m, only : default_real, double_precision
  use metadata_m, only : metadata_t
  use mini_batch_m, only : mini_batch_t
  use network_configuration_m, only : network_configuration_t
- use gelu_m, only : gelu_t
  use relu_m, only : relu_t
  use sigmoid_m, only : sigmoid_t
  use step_m, only : step_t
