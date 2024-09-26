222c062a4986b0cc8a457aa9db8f2eab22436c68
Damian Rouson <rouson@lbl.gov>
2024-09-25 22:39:18 -1000

    chore: mv macros to include/language-support.F90

--- src/inference_engine/trainable_engine_s.F90
+++ src/inference_engine/trainable_engine_s.F90
@@ -1,17 +1,7 @@
 ! Copyright (c), The Regents of the University of California
 ! Terms of use are as specified in LICENSE.txt
 
-#ifndef F2023_LOCALITY
-#if defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 202400)
-# define F2023_LOCALITY 1
-#endif
-#endif
-
-#ifndef F2018_LOCALITY
-#if defined(_CRAYFTN)
-# define F2018_LOCALITY 1
-#endif
-#endif
+#include "language-support.F90"
 
 submodule(trainable_engine_m) trainable_engine_s
   use assert_m, only : assert
